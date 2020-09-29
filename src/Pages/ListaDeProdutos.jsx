cef285fc6298fca6a784d9c5fe44b45ad5374b1d
Flavianny Maciel <flaviannymaciel@gmail.com>
2020-09-29 19:03:26 -0300

    Em andamento requisito2-trocando h1 por p

--- src/Pages/ListaDeProdutos.jsx
+++ src/Pages/ListaDeProdutos.jsx
@@ -11,9 +11,9 @@ class ListaDeProdutos extends Component {
   render() {
     return (
       <div>
-        <h1 data-testid="home-initial-message">
+        <p data-testid="home-initial-message">
           Digite algum termo de pesquisa ou escolha uma categoria.
-        </h1>
+        </p>
         <CampoBusca />
       </div>
     );

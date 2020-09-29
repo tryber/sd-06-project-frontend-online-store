cb06d6847e36a76ac82c17b702cdc5d732f9096b
Luiz Mello - Turma 06 <luizmacmillan@gmail.com>
2020-09-29 18:47:09 -0300

    :sparkles: Shopping cart button and page implemented
    Created button and component to Shopping cart functionality.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import './App.css';
+import ShoppingCart from './components/ShoppingCart';
 import ProductListing from './components/ProductListing';
 
 function App() {
   return (
     <BrowserRouter>
       <Switch>
+        <Route exact path="/ShoppingCart" component={ ShoppingCart } />
         <Route exact path="/" component={ ProductListing } />
       </Switch>
     </BrowserRouter>

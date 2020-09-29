33662bda01939584e4eb07371f9831537899b99f
Victor-Mateus-Ferreira <victormateuslf@gmail.com>
2020-09-29 17:46:29 -0300

    :zap: task 0 implementing function getProductsFromCategoryAndQuery ES1

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
+import logo from './logo.svg';
 import './App.css';
-import * as api from './services/api';
 
 function App() {
   return (
--- src/services/api.js
+++ src/services/api.js
@@ -2,12 +2,12 @@
 export function getCategories() {
   const urlApi = 'https://api.mercadolibre.com/sites/MLB/categories';
   return fetch(urlApi)
-    .then((response) => response.json())
+    .then((response) => response.json());
 }
 
 export function getProductsFromCategoryAndQuery(categoryId, query) {
-  const endpoint = `https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}&q=${query}`
+  const endpoint = `https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}&q=${query}`;
 
   return fetch(endpoint)
-    .then((response) => response.json())
+    .then((response) => response.json());
 }

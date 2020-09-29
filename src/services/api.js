a4e0de654b8ab248b3e3c79140dbcd018e2a4ab6
Thales Rangel <thalesgr@gmail.com>
2020-09-29 18:08:19 -0300

    api done

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,27 @@
 export async function getCategories() {
   // Implemente aqui
+  const endpoint = 'https://api.mercadolibre.com/sites/MLB/categories';
+  const categories = await fetch(endpoint);
+  const categoriesObject = await categories.json();
+  return categoriesObject;
 }
 
-export async function getProductsFromCategoryAndQuery(/* categoryId, query */) {
+export async function getProductsFromCategoryAndQuery(categoryId, query) {
   // Implemente aqui! Quando o fizer, descomente os parâmetros que essa função recebe
+  const url = 'https://api.mercadolibre.com/sites/MLB/search?';
+  if (categoryId && query) {
+    const products = await fetch(`${url}category=${categoryId}&q=${query}`);
+    const productsObject = await products.json();
+    return productsObject;
+  }
+  if (categoryId && query === undefined) {
+    const products = await fetch(`${url}category=${categoryId}`);
+    const productsObject = await products.json();
+    return productsObject;
+  }
+  if (query && categoryId === undefined) {
+    const products = await fetch(`${url}q=${query}`);
+    const productsObject = await products.json();
+    return productsObject;
+  }
 }

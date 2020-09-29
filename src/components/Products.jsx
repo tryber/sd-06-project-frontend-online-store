5354494060d74309bfbb0ad8e1f7c109f8ad949f
Luciano Scalfone <luciano.scalfone@gmail.com>
2020-09-29 20:13:50 -0300

    Issues req 4

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -27,7 +27,7 @@ class Products extends Component {
     return (
       <nav>
         <ul>
-          {category.map(name => <li data-testid="category" key={name.id}>{name.name}</li>)}
+          {category.map(names => <li data-testid="category" key={names.id}>{names.name}</li>)}
         </ul>
       </nav>
     );

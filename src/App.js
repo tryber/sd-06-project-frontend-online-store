8c3455154f04b2e469d87ddad2b0a024c25864ca
Aloizio Borges Santos Coelho <aloiziobsc@gmail.com>
2020-09-29 17:52:38 -0300

    requisito1 versão2

--- src/App.js
+++ src/App.js
@@ -1,12 +1,1 @@
-import React from 'react';
-import * as api from './services/api' 
-import './App.css';
 
-function App() {
-  return (
-    <div>
-    </div>
-  );
-}
-
-export default App;
--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,8 @@
 export async function getCategories() {
   const urlEndPoint = 'https://api.mercadolibre.com/sites/MLB/categories';
   const request = fetch(urlEndPoint)
-    .then(response => response.json())
-    return request;
+    .then((response) => response.json());
+  return request;
 }
 
 export async function getProductsFromCategoryAndQuery(/* categoryId, query */) {

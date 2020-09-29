44ef46861a9940e10b6cb4102f4dcec6c6032545
jederson <jmacedo.ti@gmail.com>
2020-09-29 19:08:31 -0300

    retornando ao estado inicial

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Search from './components/Search';
-import ShoppingCart from './components/ShoppingCart';
 
 function App() {
   return (
     <BrowserRouter>
       <Switch>
-        <Route exact path="/" component={ Search } />
-        <Route exact path="/ShoppingCart" component={ ShoppingCart } />
+        <Route path="/" component={ Search } />
       </Switch>
     </BrowserRouter>
   );

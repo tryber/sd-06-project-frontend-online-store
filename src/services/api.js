40002603d4ac513d7727f472709480aa45f50a54
William Gomes <williamjog@hotmail.com>
2020-09-29 16:55:56 -0300

    1º Requisito Teste

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,11 @@
 export async function getCategories() {
-  // Implemente aqui
+  fetch('https://api.mercadolibre.com/sites/MLB/categories')
+    .then((response) => console.log(response.json())
+      .then((data) => data));
 }
 
-export async function getProductsFromCategoryAndQuery(/* categoryId, query */) {
-  // Implemente aqui! Quando o fizer, descomente os parâmetros que essa função recebe
+export async function getProductsFromCategoryAndQuery(categoryId, query) {
+  fetch(`https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}&q=${query}`)
+    .then((response) => response.json())
+    .then((data) => data);
 }

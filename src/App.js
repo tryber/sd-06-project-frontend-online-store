f7534c78da173d8aac49e79935c1bb245c554418
Gustavo Oliva <gustavociviloliva@gmail.com>
2020-09-29 18:57:40 -0300

    Add search button and empty page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Home from './components/Home';
+import ShoppingCart from './components/ShoppingCart';
 
 function App() {
   return (
     <BrowserRouter>
       <Switch>
         <Route exact path="/" component={ Home } />
+        <Route exact path="/shopping-cart" component={ ShoppingCart } />
       </Switch>
     </BrowserRouter>
   );
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,15 +1,22 @@
 import React from 'react';
 import '../styles/Home.css';
+import { Link } from 'react-router-dom';
+import Carrinho from '../imgs/carrinho.png';
 
 class Home extends React.Component {
   render() {
     return (
-      <div data-testid="home-initial-message" className="home">
-        <input
-          type="text"
-          className="home-input"
+      <div className="home">
+        <div data-testid="home-initial-message" className="search-bar">
+          <input
+            type="text"
+            className="home-input"
           // onKeyUp={ (event) => event.keyCode === 13 ? console.log('evento funcionando') : '' }
-        />
+          />
+          <Link to="/shopping-cart">
+            <img data-testid="shopping-cart-button" src={ Carrinho } alt="Carrinho" />
+          </Link>
+        </div>
         <span className="home-span">
           Digite algum termo de pesquisa ou escolha uma categoria.
         </span>

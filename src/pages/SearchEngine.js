9b582adec05b4f05af3c6756f03390ddd0cad62e
ilanribeiro <ilanribeiro@gmail.com>
2020-09-29 17:54:21 -0300

    requirement 2 completed - SearchEngine - missing code review - ajusting line 7 from SearchEngine

--- src/pages/SearchEngine.js
+++ src/pages/SearchEngine.js
@@ -4,7 +4,11 @@ function SearchEngine() {
   return (
     <div>
       <input type="text" />
-      <p data-testid="home-initial-message">Digite algum termo de pesquisa ou escolha uma categoria.</p>
+      <p
+        data-testid="home-initial-message"
+      >
+        Digite algum termo de pesquisa ou escolha uma categoria.
+      </p>
     </div>
   );
 }

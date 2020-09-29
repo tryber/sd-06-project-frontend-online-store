5784669212dc7d468be8521161d53c03d03328ff
rafaelgelatti <rafael.gelatti@gmail.com>
2020-09-29 17:48:33 -0300

    Corrigindo lint req 2

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <h2 data-testid="home-initial-message">Digite algum termo de pesquisa ou escolha uma categoria.</h2>
+    <h2 data-testid="home-initial-message">
+      Digite algum termo de pesquisa ou escolha uma categoria.
+    </h2>
   );
 }
 

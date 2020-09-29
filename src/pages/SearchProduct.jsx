0d7370b0955b24de313e3b8a46623db1ec724e45
alcantara2509 <alcantara2509@gmail.com>
2020-09-29 20:02:12 -0300

    #3: Shopping Cart page and button created

--- src/pages/SearchProduct.jsx
+++ src/pages/SearchProduct.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import { getCategories } from '../services/api';
 
 // comment to commit
@@ -43,6 +44,7 @@ class SearchProduct extends Component {
             Digite algum termo de pesquisa ou escolha uma categoria.
           </h1>
         </form>
+        <Link to="/shopping-cart" data-testid="shopping-cart-button">cart</Link>
       </div>
     );
   }

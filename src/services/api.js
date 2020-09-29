0ed42f51b9f0c07ccd6167cb7b55428bd8ffa506
Gui-Alucard <guilhermeanv7822@gmail.com>
2020-09-29 16:30:04 -0300

    :rocket: api.js implemented
    Both getCategories() and getProductsFromCategoryAndQuery() are implemented with succsses!

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,13 @@
 export async function getCategories() {
-
+  const url = 'https://api.mercadolibre.com/sites/MLB/categories';
+  const response = await fetch(url);
+  const data = await response.json();
+  return data;
 }
 
-export async function getProductsFromCategoryAndQuery(/* categoryId, query */) {
-  // Implemente aqui! Quando o fizer, descomente os parâmetros que essa função recebe
+export async function getProductsFromCategoryAndQuery(categoryId, query) {
+  const catQuery = `https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}&q=${query}`;
+  const response = await fetch(catQuery);
+  const data = await response.json();
+  return data;
 }

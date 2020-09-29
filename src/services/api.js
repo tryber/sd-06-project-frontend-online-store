9f796dddd356bdacae131508031dead622760464
Matheus Coutinho <matheuscavalcante88@hotmail.com>
2020-09-29 16:41:52 -0300

    getCategories function

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,12 @@
 export async function getCategories() {
-  // Implemente aqui
+  const endpoint = 'https://api.mercadolibre.com/sites/MLB/categories';
+
+  const allCategories = await fetch(endpoint).then((r) => r.json());
+
+  return allCategories;
 }
+
+// 
 
 export async function getProductsFromCategoryAndQuery(/* categoryId, query */) {
   // Implemente aqui! Quando o fizer, descomente os parâmetros que essa função recebe

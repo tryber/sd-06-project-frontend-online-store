16c7ef4dc73f1eeb3bca052e2b68bbfd9f22b6ef
FPhilipi <fabiphilipi@gmail.com>
2020-09-29 18:03:52 -0300

    #2: fixed indentantion and others

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
-import logo from './logo.svg';
 import './App.css';
 import SearchProduct from './pages/SearchProduct';
-
 
 function App() {
   return (
     <BrowserRouter>
-    <Route path="/" component={SearchProduct}/>    
+      <Route exact path="/" component={ SearchProduct } />
     </BrowserRouter>
   );
 }
--- src/pages/SearchProduct.js
+++ src/pages/SearchProduct.js
@@ -1,17 +1,14 @@
 import React, { Component } from 'react';
 
 class SearchProduct extends Component {
-
-    render(){
-
-        return (
-            <form>
-                <input type="text">
-                </input>
-                <h1 data-testid="home-initial-message">Digite algum termo de pesquisa ou escolha uma categoria.</h1>
-            </form>
-        )
-    }
+  render() {
+    return (
+      <form>
+        <input type="text" />
+        <h1 data-testid="home-initial-message">Digite algum termo de pesquisa ou escolha uma categoria</h1>
+      </form>
+    );
+  }
 }
 
 export default SearchProduct;

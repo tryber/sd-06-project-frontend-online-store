99eb9bf77feb642e2848b1bb507162d1a2e6b506
ilanribeiro <ilanribeiro@gmail.com>
2020-09-29 17:44:21 -0300

    requirement 2 completed - SearchEngine - missing code review

---
+++ src/pages/SearchEngine.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function SearchEngine() {
+  return (
+    <div>
+      <input type="text" />
+      <p data-testid="home-initial-message">Digite algum termo de pesquisa ou escolha uma categoria.</p>
+    </div>
+  );
+}
+
+
+export default SearchEngine;

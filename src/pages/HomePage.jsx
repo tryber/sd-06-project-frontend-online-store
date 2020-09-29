4e33a27d7c9959f7c62309028c542ecff29eb349
Clayton Miguel Da Silva <claytonmiguel1805@gmail.com>
2020-09-29 18:16:21 -0300

    req - 03

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 class HomePage extends Component {
   
   render() {
     return (
       <div>
+        <Link data-testid="shopping-cart-button" to="/cart">CART</Link>
         <h1 data-testid="home-initial-message">Digite algum termo de pesquisa ou escolha uma categoria.</h1>
       </div>
     );

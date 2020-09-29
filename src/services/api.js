dfecb0632afa70173cec5ee5213582c8cb319dcd
William Gomes <williamjog@hotmail.com>
2020-09-29 17:02:57 -0300

    1º Requisito OK | funcoes API

--- src/services/api.js
+++ src/services/api.js
@@ -1,11 +1,11 @@
 export async function getCategories() {
-  fetch('https://api.mercadolibre.com/sites/MLB/categories')
-    .then((response) => console.log(response.json())
-      .then((data) => data));
+  return fetch('https://api.mercadolibre.com/sites/MLB/categories')
+    .then((response) => response.json())
+    .then((data) => data);
 }
 
 export async function getProductsFromCategoryAndQuery(categoryId, query) {
-  fetch(`https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}&q=${query}`)
+  return fetch(`https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}&q=${query}`)
     .then((response) => response.json())
     .then((data) => data);
 }

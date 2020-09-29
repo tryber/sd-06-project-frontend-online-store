f5f8b7f9211afc878d6695438d98b803c809d171
DekoPossas <andrepossaster@gmail.com>
2020-09-29 18:54:41 -0300

    criei um button direcionado para a pagina do carrinho

--- src/services/ShopList.js
+++ src/services/ShopList.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 class ShopList extends React.Component {
   render() {
     return (
       <div data-testid="home-initial-message">
+        <input type="text" />
         Digite algum termo de pesquisa ou escolha uma categoria.
+        <button type="button" data-testid="shopping-cart-button">
+          <Link to="/cart">Carrinho</Link>
+        </button>
       </div>
     );
   }

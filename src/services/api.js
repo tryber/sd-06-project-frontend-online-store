bb4f56e559b8fdfb970e86e313d1d684842997e3
MarioDuarte <marioduarte.engenheiro@gmail.com>
2020-09-29 18:45:06 -0300

    Requiriment1: Lint changes

--- src/services/api.js
+++ src/services/api.js
@@ -1,11 +1,11 @@
 export async function getCategories() {
   const urlCategories = 'https://api.mercadolibre.com/sites/MLB/categories';
   return fetch(urlCategories)
-    .then(response => response.json());
+    .then((response) => response.json());
 }
 
-export async function getProductsFromCategoryAndQuery( categoryId, query ) {
+export async function getProductsFromCategoryAndQuery(categoryId, query) {
   const urlProducts = `https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}&q=${query}`;
   return fetch(urlProducts)
-    .then(response => response.json());
+    .then((response) => response.json());
 }

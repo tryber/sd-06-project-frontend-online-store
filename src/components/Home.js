bf8fb8dc0465f706f36def3ac4ffa76298ea8b70
Tiago Berwanger <berwangertiago@gmail.com>
2020-09-29 19:25:38 -0300

    Requirement 4: creating input and button

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,9 +3,15 @@ import React from 'react';
 class Home extends React.Component {
   render() {
     return (
-      <p data-testid="home-initial-message">
-        Digite algum termo de pesquisa ou escolha uma categoria.
-      </p>
+      <div>
+        <p data-testid="home-initial-message">
+          Digite algum termo de pesquisa ou escolha uma categoria.
+        </p>
+        <div>
+          <input data-testid="query-input" type="text" />
+          <button data-testid="query-button">Pesquisar</button>
+        </div>
+      </div>
     );
   }
 }

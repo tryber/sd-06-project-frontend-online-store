532069a07b6a95d159e74996e303c2d2e344d765
Ana Higo <anahigo.arq@gmail.com>
2020-09-29 17:36:08 -0300

    Implemetation of Mercado Livre API

--- src/services/api.js
+++ src/services/api.js
@@ -2,11 +2,11 @@ export async function getCategories() {
   const url = 'https://api.mercadolibre.com/sites/MLB/categories';
   const result = await fetch(url);
   const categoriesObj = await result.json();
-  
+
   return categoriesObj;
 }
 
-export async function getProductsFromCategoryAndQuery( categoryId, query) {
+export async function getProductsFromCategoryAndQuery(categoryId, query) {
   const result = await fetch(`https://api.mercadolibre.com/sites/MLB/search?category=$${categoryId}_ID&q=$${query}`);
   const productObj = await result.json();
 

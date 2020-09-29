aae26dff1e3d666fd435689779d897dfea01edde
Elias Carvalho <eliascarvalho01@hotmail.com>
2020-09-29 17:07:07 -0300

    :rotating_light: - fixing code style

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
 import Home from './pages/Home';
 import NotFound from './pages/NotFound';
 
-class App extends Component {
+class App extends React.Component {
   render() {
     return (
       <Router>

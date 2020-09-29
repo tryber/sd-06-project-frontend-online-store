2fb59e276ac5b49a8242c42549ac9bb0b97fda4d
MarioDuarte <marioduarte.engenheiro@gmail.com>
2020-09-29 18:12:39 -0300

    Requirement1: Implement API

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,11 @@
 export async function getCategories() {
-  // Implemente aqui
+  const urlCategories = 'https://api.mercadolibre.com/sites/MLB/categories';
+  return fetch(urlCategories)
+    .then(response => response.json());
 }
 
-export async function getProductsFromCategoryAndQuery(/* categoryId, query */) {
-  // Implemente aqui! Quando o fizer, descomente os parâmetros que essa função recebe
+export async function getProductsFromCategoryAndQuery( categoryId, query ) {
+  const urlProducts = `https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}&q=${query}`;
+  return fetch(urlProducts)
+    .then(response => response.json());
 }

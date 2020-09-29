0d44c5bae4a29629df7c75e89910b891ee9ef505
danieleperse <danieleperse3@gmail.com>
2020-09-29 16:25:32 -0300

    teste commit erro l 12

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
         <img src={ logo } className="App-logo" alt="logo" />
         <p>
           Edit
-          {'teste1'}
+          {' '}
           <code>src/App.js</code>
           {' '}
           and save to reload.

36ed670dfc2b0f60a7649c6deb60beca93862107
Rods <rods.leite27@gmail.com>
2020-09-29 17:08:56 -0300

    Requisito 1

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,11 @@
 export async function getCategories() {
-  // Implemente aqui
+  return fetch('https://api.mercadolibre.com/sites/MLB/categories')
+    .then((categorias) => categorias.json());
 }
 
-export async function getProductsFromCategoryAndQuery(/* categoryId, query */) {
-  // Implemente aqui! Quando o fizer, descomente os parâmetros que essa função recebe
+export async function getProductsFromCategoryAndQuery(categoryId, query) {
+  const url = `https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}&q=${query}`;
+  return fetch(url).then((categorias) => categorias.json());
 }
+
+//"https://api.mercadolibre.com/sites/MLB/search?category=MLB1055&q=Motorola"

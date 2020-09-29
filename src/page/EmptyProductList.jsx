fd6d4c2cd5121e79a0fdb61632cad4a9210bdf84
Jean Machado <jeanvictor.arq@gmail.com>
2020-09-29 20:59:32 -0300

    recuperando arquivos

---
+++ src/page/EmptyProductList.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class EmptyProductList extends React.Component {
+  render() {
+    return (
+      <div>
+        <input />
+        <p data-testid="home-initial-message">Digite algum termo de pesquisa ou escolha uma categoria.</p>
+      </div>
+    )
+  }
+}
+
+export default EmptyProductList;

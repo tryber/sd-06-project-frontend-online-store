7ffee74c45bd071e3723896d38ec03d1cb143615
Daniel Moura <darthur.moura@gmail.com>
2020-09-29 17:45:44 -0300

    Req 1: Implementaçao requisito 1

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,17 @@
 export async function getCategories() {
-  // Implemente aqui
+  const endpoint = {api: 'https://api.mercadolibre.com/sites/MLB/categories'};
+
+  let response = await fetch(endpoint.api);
+  let data = await response.json();
+
+  return data;
 }
 
-export async function getProductsFromCategoryAndQuery(/* categoryId, query */) {
-  // Implemente aqui! Quando o fizer, descomente os parâmetros que essa função recebe
+export async function getProductsFromCategoryAndQuery(categoryId, query) {
+  const apiUrl = 'https://api.mercadolibre.com/sites/MLB/search?category=';
+
+  let response = await fetch(`${apiUrl}${categoryId}&q=${query}`);
+  let data = await response.json();
+
+  return data;
 }

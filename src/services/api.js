72e1b868397fdb04ffef0c8b9aa91ca76a7e2ece
Marcelo Pessini <marceloppessini@gmail.com>
2020-09-29 17:30:08 -0300

    FEAT: API request category  implemented

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,9 @@
 export async function getCategories() {
-  // Implemente aqui
+  const response = await fetch('https://api.mercadolibre.com/sites/MLB/categories');
+  const result = await response.json();
+  return result;
 }
+
 
 export async function getProductsFromCategoryAndQuery(/* categoryId, query */) {
   // Implemente aqui! Quando o fizer, descomente os parâmetros que essa função recebe

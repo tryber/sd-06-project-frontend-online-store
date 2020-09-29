0b7f577ab2a387a6e667590e4646deb387fa06c1
Victor-Mateus-Ferreira <victormateuslf@gmail.com>
2020-09-29 16:42:02 -0300

    :sparkles: task 0 - implementing api request

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,8 @@
-export async function getCategories() {
-  // Implemente aqui
+
+export function getCategories() {
+  const urlApi = 'https:api.mercadolibre.com/sites/MLB/categories';
+  return fetch(urlApi)
+    .then((response) => response.json())
 }
 
 export async function getProductsFromCategoryAndQuery(/* categoryId, query */) {

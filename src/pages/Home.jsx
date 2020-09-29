04c92b5e8a7ea1851f79e0885c84e6bebc58a95a
Lucas Santiago Gomide <lucas.sgomide92@gmail.com>
2020-09-29 19:12:37 -0300

    Começando requisito

---
+++ src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import SearchBar from '../components/SearchBar';
+
+
+class Home extends React.Component {
+  render () {
+    return (
+      <div>
+        <SearchBar />
+      </div>
+    )
+  }
+}
+
+export default Home;

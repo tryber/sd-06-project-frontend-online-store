c7479b2d22e3bc73c88f711f05ed75218e55fcd0
isabellavjs <isabellavjs@gmail.com>
2020-09-29 19:38:25 -0300

    Requirements 5 - product list

---
+++ src/components/ProductCard.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class ProductCard extends React.ComponentComponent {
+  render() {
+    return (
+      <div data-testid="product">
+        Produto 1
+      </div>
+    );
+  }
+}
+
+export default ProductCard;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,18 +1,21 @@
 import React from 'react';
 
-function Home() {
-  return (
-    <div>
-      <form>
-        <input type="text" name="research" />
-      </form>
-      <p
-        data-testid="home-initial-message"
-      >
-        Digite algum termo de pesquisa ou escolha uma categoria.
-      </p>
-    </div>
-  );
+
+class Home extends React.Component {
+  render() {
+    return (
+      <div>
+        <form>
+          <input data-testid="query-input" type="text" name="research" />
+        </form>
+        <p
+          data-testid="home-initial-message"
+        >
+          Digite algum termo de pesquisa ou escolha uma categoria.
+        </p>
+      </div>
+    )
+  }
 }
 
 export default Home;

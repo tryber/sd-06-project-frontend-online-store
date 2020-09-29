9b5c6200eb0a65d676cdeba0ead50a27ab181c6f
Jean Machado <jeanvictor.arq@gmail.com>
2020-09-29 18:46:32 -0300

    updating branch

--- src/component/EmptyProductList.jsx
+++ src/component/EmptyProductList.jsx
@@ -2,12 +2,7 @@ import React from 'react';
 
 class EmptyProductList extends React.Component {
   render() {
-    return (
-      <div>
-        <input type="text" name="input-initial" ></input>
-        <label htmlFor="input-initial" data-testid="home-initial-message">Digite algum termo de pesquisa ou escolha uma categoria.</label>
-      </div>
-    )
+    return <p data-testid="home-initial-message">Digite algum termo de pesquisa ou escolha uma categoria.</p>
   }
 }
 

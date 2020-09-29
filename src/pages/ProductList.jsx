dfc4b72158938be19010b899a437c9fcd1f7e61e
Ricardo Ribeiro <ralexandre11.gmail.com>
2020-09-29 17:59:49 -0300

    Feature Requiriment 02

---
+++ src/pages/ProductList.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class ProductList extends React.Component {
+  render () {
+    return (
+      <div>
+        <input type="text" name="" id=""></input>
+        <h1 data-testid="home-initial-message">
+          Digite algum termo de pesquisa ou escolha uma categoria.
+        </h1>
+      </div>
+    );
+  }
+}
+
+export default ProductList;

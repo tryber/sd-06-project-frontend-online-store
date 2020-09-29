ebabbf2fc1df1f73c082efde867ef5a136bde764
jederson <jmacedo.ti@gmail.com>
2020-09-29 19:11:47 -0300

    carrinho de compras

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Search from './components/Search';
+import ShoppingCart from './components/ShoppingCart'
 
 function App() {
   return (
     <BrowserRouter>
       <Switch>
-        <Route path="/" component={ Search } />
+        <Route exact path="/" component={ Search } />
+        <Route exact path="/ShoppingCart" component={ ShoppingCart } />
       </Switch>
     </BrowserRouter>
   );
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import ShoppingCart from './ShoppingCart'
 import { Link } from 'react-router-dom'
 
 class Search extends Component {

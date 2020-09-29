1b16273256d8cc8aad09217e71e48836f299530b
Ana Flávia <anaflvia@gmail.com>
2020-09-29 17:02:17 -0300

    corrigindo lint

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,16 +1,18 @@
 import React from 'react';
 
-class Home extends React.Component {
-  render() {
-    return (
-      <div>
-        <form>
-          <input type="text" name="research" />
-        </form>
-        <p data-testid="home-initial-message">Digite algum termo de pesquisa ou escolha uma categoria.</p>
-      </div>
-    )
-  }
+function Home() {
+  return (
+    <div>
+      <form>
+        <input type="text" name="research" />
+      </form>
+      <p
+        data-testid="home-initial-message"
+      >
+        Digite algum termo de pesquisa ou escolha uma categoria.
+      </p>
+    </div>
+  );
 }
 
 export default Home;

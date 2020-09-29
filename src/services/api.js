becb13c22bef57435abdeb8d614f8237766c43cd
Rods <rods.leite27@gmail.com>
2020-09-29 17:13:48 -0300

    Erros EsLint

--- src/services/api.js
+++ src/services/api.js
@@ -7,5 +7,3 @@ export async function getProductsFromCategoryAndQuery(categoryId, query) {
   const url = `https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}&q=${query}`;
   return fetch(url).then((categorias) => categorias.json());
 }
-
-//"https://api.mercadolibre.com/sites/MLB/search?category=MLB1055&q=Motorola"

cb9f04746808b4340604e62bbfa9d531eea7e6be
wfelipefarina <wfelipefarina@gmail.com>
2020-09-29 18:33:19 -0300

    :bug: HomePage
    Fixed ESLint at App.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import './App.css';
 function App() {
   return (
     <BrowserRouter>
-      <Route path="/" component={HomePage} />
+      <Route path="/" component={ HomePage } />
     </BrowserRouter>
   );
 }

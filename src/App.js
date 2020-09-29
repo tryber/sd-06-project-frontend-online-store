87543ce6bfb744c0424d42f3fb828e5ebb5514d1
Matheuscara <matheuscara2000@gmail.com>
2020-09-29 15:55:59 -0300

    Repair (Erro - Pure functio)

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
     <div className="App">
       <Router>
         <Switch>
-          <Route exact path="/" component={homeList} />
+          <Route exact path="/" component={ homeList } />
         </Switch>
       </Router>
     </div>
--- src/pages/homeList.js
+++ src/pages/homeList.js
@@ -1,16 +1,14 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class homeList extends Component {
-  render() {
-    return (
-      <div>
-        <input />
-        <h1 data-testid="home-initial-message">
-          Digite algum termo de pesquisa ou escolha uma categoria.
-        </h1>
-      </div>
-    );
-  }
+function homeList() {
+  return (
+    <div>
+      <input />
+      <h1 data-testid="home-initial-message">
+        Digite algum termo de pesquisa ou escolha uma categoria.
+      </h1>
+    </div>
+  );
 }
 
 export default homeList;

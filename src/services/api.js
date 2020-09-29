43d348af8eb5737eab9bc5a31754034636ff2a54
Matheuscara <matheuscara2000@gmail.com>
2020-09-29 18:01:59 -0300

    Fix

--- src/services/api.js
+++ src/services/api.js
@@ -1,11 +1,11 @@
 export async function getCategories() {
   const url = 'https://api.mercadolibre.com/sites/MLB/categories';
   return fetch(url)
-    .then((res) => res.json())
+    .then((res) => res.json());
 }
 
 export async function getProductsFromCategoryAndQuery(categoryId, query) {
   const url = `https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}&q=${query}`;
   return fetch(url)
-    .then((res) => res.json())
+    .then((res) => res.json());
 }

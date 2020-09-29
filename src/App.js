c4359e552c5108752f0b9b6aeedc1b7b80cfcc54
Marcelo Pessini <marceloppessini@gmail.com>
2020-09-29 18:18:44 -0300

    FIX ESLint Correction

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import './App.css';
-import {BrowserRouter, Route, Switch } from 'react-router-dom';
-import Home from './components/Home'
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Home from './components/Home';
 // import * as api from './services/api';
 
 function App() {
   return (
     <BrowserRouter>
       <Switch>
-        <Route exact path="/" component={Home} />
+        <Route exact path="/" component={ Home } />
       </Switch>
     </BrowserRouter>
   );

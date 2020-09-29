bc43156f20f3e2733438f955e83a76186d5163a1
Elias Carvalho <eliascarvalho01@hotmail.com>
2020-09-29 17:15:43 -0300

    :rotating_light: - fixing code style

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
 import Home from './pages/Home';
 import NotFound from './pages/NotFound';
 
-class App extends React.Component {
+class App extends Component {
   render() {
     return (
       <Router>
         <Switch>
-          <Route exact path="/" component={Home} />
-          <Route component={NotFound} />
+          <Route exact path="/" component={ Home } />
+          <Route component={ NotFound } />
         </Switch>
       </Router>
     );
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,14 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-class Home extends React.Component {
+class Home extends Component {
   render() {
     return (
       <div>
-        <input type="text"/>
-        <h1 data-testid="home-initial-message">Digite algum termo de pesquisa ou escolha uma categoria.</h1>
+        <input type="text" />
+        <span
+          data-testid="home-initial-message">
+          Digite algum termo de pesquisa ou escolha uma categoria.
+        </span>
       </div>
     );
   }

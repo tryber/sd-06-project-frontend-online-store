849d3370db5361a31a8e89baa26fc2af14e250bf
FPhilipi <fabiphilipi@gmail.com>
2020-09-29 17:43:03 -0300

    #2: Search Product

--- src/App.js
+++ src/App.js
@@ -1,29 +1,15 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
+import SearchProduct from './pages/SearchProduct';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={ logo } className="App-logo" alt="logo" />
-        <p>
-          Edit
-          {' '}
-          <code>src/App.js</code>
-          {' '}
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+    <Route path="/" component={SearchProduct}/>    
+    </BrowserRouter>
   );
 }
 
---
+++ src/pages/SearchProduct.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class SearchProduct extends Component {
+
+    render(){
+
+        return (
+            <form>
+                <input type="text">
+                </input>
+                <h1 data-testid="home-initial-message">Digite algum termo de pesquisa ou escolha uma categoria.</h1>
+            </form>
+        )
+    }
+}
+
+export default SearchProduct;

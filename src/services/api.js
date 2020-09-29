b27f43bf6793c64e7572f3a3862abda63c9d0dc7
Lais Gurgel <laisgurgelf@gmail.com>
2020-09-29 17:08:13 -0300

    Codeclimate changes

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 export async function getCategories() {
-  const url = 'https://api.mercadolibre.com/sites/MLB/categories'
+  const url = 'https://api.mercadolibre.com/sites/MLB/categories';
   return fetch(url)
-  .then(response => response.json())
+  .then((response) => response.json());
 }
 
 export async function getProductsFromCategoryAndQuery(/* categoryId, query */) {

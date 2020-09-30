1fe68e16a97a74ce6bf24151d283a7634ee70c77
kissyla <kissylamottapereira@gmail.com>
2020-09-30 14:53:30 -0300

    Req 3:Criar o botão de carrinho de compras na tela principal, de listagem de produtos, e criar uma tela para o carrinho de compras, inicialmente vazio

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import PagInicial from './PagInicial';
+import CarrinhoCompras from './services/CarrinhoCompras';
 
 
 function App() {
   return (
     <BrowserRouter>
       <Switch>
-        <Route path="/" component={ PagInicial } />
+        <Route exact path="/" component={ PagInicial } />
+        <Route path="/CarrinhoCompras" component={ CarrinhoCompras } />
       </Switch>
     </BrowserRouter>
   );

7c6012fbd81331062cb0589df3f2e0bafdadbafc
rafMewtwo <ponciano.raf@gmail.com>
2020-09-30 18:42:26 -0300

    feat: add images for buttons(Link) cart and back. add image for empty-cart too

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import cart from './../img/cart.png'
 
 export default class Home extends Component {
   render() {
@@ -8,7 +9,7 @@ export default class Home extends Component {
         <label htmlFor="search-input" data-testid="home-initial-message">Digite algum termo de pesquisa ou escolha uma categoria.
           <input id="search-input" />
         </label>
-        <Link to="/cart" data-testid="shopping-cart-button">Carrinho</Link>
+        <Link to="/cart" data-testid="shopping-cart-button"><img src={ cart } alt="imagem cart" width="30px"/></Link>
       </div>
     );
   }
--- src/pages/ShoppingCart.jsx
+++ src/pages/ShoppingCart.jsx
@@ -1,13 +1,21 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import cart from './../img/cart.png';
+import back from './../img/back.png';
+import empytCart from './../img/empty-cart.png';
 
 class ShoppingCart extends Component {
     render() {
         return (
             <div>
-                <span>Voltar</span>
-                <Link to="/cart">Carrinho</Link>
-                <span data-testid="shopping-cart-empty-message">Seu carrinho está vazio</span>
+                <Link to="/"><img src={ back } alt="imagem voltar" width="30px" /></Link>
+                <br />
+                <Link to="/cart"><img src={ cart } alt="imagem cart" width="30px" /><span>Carrinho de Compras</span></Link>
+                <br />
+                <div className="empty-cart">
+                    <img src={ empytCart } alt="carrinho vazio" width="100px" />
+                    <span data-testid="shopping-cart-empty-message">Seu carrinho está vazio</span>
+                </div>
             </div>
         );
     }

9e51183aac3e9d46359adf147e497f48f590ceb0
Mariano Ariel Diaz Elsztain <made@poli.ufrj.br>
2020-09-30 11:42:58 -0300

    [Group 22] FEAT: category listing

---
+++ src/components/CategoryList.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import * as api from '../services/api';
+
+class CategoryList extends React.Component {
+  constructor() {
+    super();
+    this.fetchCategoryList = this.fetchCategoryList.bind(this);
+    this.state = { categories: [] };
+  }
+
+  componentDidMount() {
+    this.fetchCategoryList();
+  }
+
+  async fetchCategoryList() {
+    const categoryList = await api.getCategories();
+    this.setState({ categories: categoryList });
+  }
+
+  render() {
+    const { categories } = this.state;
+    return (
+      <ul>
+        {categories
+          .map(category => {
+            return (<li key={ category.id } data-testid="category">{category.name}</li>);
+          })}
+      </ul>
+    );
+  }
+}
+
+export default CategoryList;

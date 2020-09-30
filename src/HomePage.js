624ee50b29b4e24be543e0fd3dedf48924ef043b
Rick Sousa <rickssti@outlook.com>
2020-09-30 03:52:25 -0300

    :rocket: Add shopping cart
    the shopping cart button and the respective component were created

--- src/HomePage.js
+++ src/HomePage.js
@@ -1,12 +1,21 @@
 import React, { Component } from 'react';
 
+import { Link } from 'react-router-dom';
+
+
 class HomePage extends Component {
   render() {
     return (
-      <p data-testid="home-initial-message">
-        Digite algum termo de pesquisa ou escolha uma categoria.
-        {/* teste */}
-      </p>
+      <header>
+        <p data-testid="home-initial-message">
+          Digite algum termo de pesquisa ou escolha uma categoria.
+        </p>
+        <Link to="/shopping-cart">
+          <button data-testid="shopping-cart-button" type="button">
+            Cart
+          </button>
+        </Link>
+      </header>
     );
   }
 }

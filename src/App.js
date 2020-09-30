e6624d462135792a3dac498d0ecf5af8da0aa157
VitorSSMoreira <vitorssmoreira@gmail.com>
2020-09-29 21:26:01 -0300

    requirement 3 - CC

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import Cart from './components/Cart.jsx';
-import Home from './components/Home.jsx';
+import Cart from './components/Cart';
+import Home from './components/Home';
 import './App.css';
 
 function App() {

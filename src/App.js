83eb25034e4be86879b17b48b27d8022891564e1
Thales Rangel <thalesgr@gmail.com>
2020-09-30 20:24:34 -0300

    Upload of details page working

--- src/App.js
+++ src/App.js
@@ -3,13 +3,16 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import './App.css';
 import ShoppingCart from './components/ShoppingCart';
 import ProductListing from './components/ProductListing';
+import ProductDetails from './components/ProductDetails';
+
 
 export default function App() {
   return (
     <BrowserRouter>
       <Switch>
-        <Route exact path="/ShoppingCart" component={ ShoppingCart } />
-        <Route exact path="/" component={ ProductListing } />
+        <Route exact path="/ShoppingCart" component={ShoppingCart} />
+        <Route exact path="/" component={ProductListing} />
+        <Route exact path="/products/:category_id/:id" render={(props) => <ProductDetails {...props} />} />
       </Switch>
     </BrowserRouter>
   );
--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,13 +1,18 @@
 import React, { Component } from 'react';
+import {Link} from 'react-router-dom';
+
 
 export default class ProductCard extends Component {
   render() {
-    const { title, thumbnail, price } = this.props;
+    const {id, title, thumbnail, price, category_id } = this.props.product;
     return (
       <div data-testid="product">
         <p>{title}</p>
         <img src={ thumbnail } alt={`${title} PIC`} />
         <p>{`Price: ${price} R$`}</p>
+        <div>
+          <Link to={`/products/${category_id}/${id}`}>Detalhes do produto</Link>          
+        </div>
       </div>
     );
   }

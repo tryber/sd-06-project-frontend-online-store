84facd53415b4294f35aa552e87c43df1294ebd0
kissyla <kissylamottapereira@gmail.com>
2020-09-30 15:32:43 -0300

    Req3: corrigindo novos problemas eslint

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import PagInicial from './PagInicial';
 import CarrinhoCompras from './components/CarrinhoCompras';
 
-
 function App() {
   return (
     <BrowserRouter>
--- src/PagInicial.js
+++ src/PagInicial.js
@@ -4,11 +4,7 @@ import { Link } from 'react-router-dom';
 function PagInicial() {
   return (
     <div data-testid="home-initial-message">
-      <label>
-        Digite algum termo de pesquisa ou escolha uma categoria.
-        <input type="text" data-testid="query-input"></input>
-      </label>
-      <button type="button" data-testid="query-button"></button>
+      <h1>Digite algum termo de pesquisa ou escolha uma categoria.</h1>
       <Link to="/CarrinhoCompras" data-testid="shopping-cart-button">
         <button type="button">Carrinho de Compras</button>
       </Link>

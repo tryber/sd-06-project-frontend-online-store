729c0af942ea8c4e8d4f1b8d048f68015573cd62
Isabela Rosa <isabelarosa954@gmail.com>
2020-09-29 22:09:32 -0300

    removendo componente home duplicado

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import SearchBar from '../components/SearchBar';
 import CartButton from '../components/CartButton';
+import CategoriesList from '../components/CategoriesList';
 
 class Home extends React.Component {
   render() {
@@ -11,6 +12,7 @@ class Home extends React.Component {
         <SearchBar />
         <Link to="/carrinho">
           <CartButton />
+          <CategoriesList />
         </Link>
       </div>
     );

5159f0596456f3817b74782df887618a56ba37f0
Cyrano webdev <cyrano@cyranowebdev.com>
2020-09-30 10:11:40 -0300

    Criando pastas components e pages em src e atualizando imports.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
-import ShopList from './services/ShopList';
-import ShopCart from './services/ShopCart';
-
+import { ShopList, ShopCart } from './pages';
 import './App.css';
-// import * as API from './services/api';
 
 class App extends React.Component {
   render() {

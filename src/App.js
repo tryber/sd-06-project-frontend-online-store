2658804541d871298cc2fb0d0c2741712fe21088
Isadora Koga <isadorakoga710@gmail.com>
2020-09-30 14:51:00 -0300

    Adding folder pages

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import Home from './components/Home';
+import Home from './pages/Home';
 
 /* PR Grupo 09 */
 

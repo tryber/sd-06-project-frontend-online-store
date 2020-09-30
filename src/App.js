6125561b94ddc42ea36a2bf737d0934267e166f7
ilanribeiro <ilanribeiro@gmail.com>
2020-09-30 10:47:13 -0300

    requirement 3 completed - Shopping Cart Page - ESlint ok

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import './App.css';
 
 import SearchEngine from './pages/SearchEngine';
+import ShoppingCartPage from './pages/ShoppingCartPage';
+
 
 function App() {
   return (
     <div className="App">
-      <SearchEngine />
+      <BrowserRouter>
+        <Switch>
+          <Route path="/shopping-cart" component={ ShoppingCartPage } />
+          <Route path="/" component={ SearchEngine } exact />
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }

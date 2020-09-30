c4dcbdd90880f425038cc1ddf22e1b63eb3522c7
kissyla <kissylamottapereira@gmail.com>
2020-09-30 15:08:38 -0300

    Req3: correção eslint

--- src/services/CarrinhoCompras.js
+++ src/services/CarrinhoCompras.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
 
 class CarrinhoCompras extends Component {
-render() {
-  return(
-    <div>
-      <p data-testid="shopping-cart-empty-message">Seu carrinho está vazio</p>
-    </div>
-  );
-}
+  render() {
+    return (
+      <div>
+        <p data-testid="shopping-cart-empty-message">Seu carrinho está vazio</p>
+      </div>
+    );
+  }
 }
 
 export default CarrinhoCompras;

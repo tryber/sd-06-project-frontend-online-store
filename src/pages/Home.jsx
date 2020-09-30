1b1df49cd4de03a647f0a1c80d4a769aabc39e3f
Vinicius Buzato <viniciusbuzato@gmail.com>
2020-09-30 17:58:06 -0300

    Realocação do estado da aplicação para o componente Home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,16 +2,54 @@ import React, { Component } from 'react';
 import Header from '../components/Header';
 import Categories from '../components/Categories'
 import Products from '../components/Products';
+import * as api from '../services/api';
 
+class Home extends Component {
+  constructor() {
+    super();
 
-export default function Home() {
-  return (
-    <div>
-      <Header />
-      <div className="content">
-        <Categories />
-        <Products />
+    this.onClick = this.onClick.bind(this);
+    this.fetchProducts = this.fetchProducts.bind(this);
+    this.handleEvent = this.handleEvent.bind(this);
+
+    this.state = {
+      textInput: "", 
+      data: null,
+    };
+  }
+
+  handleEvent({ target }) {
+    const query = target.value;
+    this.setState({ textInput: query });
+  }
+
+  onClick() {
+    const query = this.state.textInput;
+    this.fetchProducts({ query });
+  }
+
+  async fetchProducts({ categoryId, query }) {
+    const fetchData = await api.getProductsFromCategoryAndQuery({ categoryId, query });
+    this.setState({ data: fetchData });
+  }
+
+  render() {
+    const { data, textInput } = this.state;
+    return (
+      <div>
+        <Header
+          inputValue={textInput}
+          handleEvent={this.handleEvent} 
+          onClick={this.onClick}
+        />
+        {/* {(data) ? console.log(data) : null} */}
+        <div className="content">
+          <Categories />
+          <Products data={data}/>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
+
+export default Home;

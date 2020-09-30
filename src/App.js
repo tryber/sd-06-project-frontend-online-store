af4e1ab3f0ae147721b29cac386daeb8eb353fa8
Ana Flávia <anaflvia@gmail.com>
2020-09-30 19:16:29 -0300

    requisito 7 feito antes do requisito 6

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import './App.css';
 import Home from './pages/Home';
 import Cart from './pages/Cart';
+import ProductDetails from './pages/ProductDetails';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Switch>
         <Route exact path="/Cart" component={ Cart } />
         <Route exact path="/" component={ Home } />
+        <Route path="/ProductDetails/:id" component={ ProductDetails } />
       </Switch>
     </BrowserRouter>
   );
---
+++ src/pages/ProductDetails.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import CartBtn from '../services/CartBtn';
+
+class ProductDetails extends React.Component {
+  render() {
+    const { location: { state: { title, thumbnail, attibutes } } } = this.props;
+    return (
+      <div>
+        <div>
+          <div>
+            <h1 data-testeid="product-detail-name">{title}</h1>
+            <img src={ thumbnail } alt={ title } />
+          </div>
+          <div>
+            <p>{ attibutes }</p>
+          </div>
+        </div>
+        <div>
+          <CartBtn />
+        </div>
+      </div>
+    );
+  }
+}
+
+ProductDetails.propTypes = {
+  location: PropTypes.shape({
+    state: PropTypes.shape({
+      title: PropTypes.string.isRequired,
+      thumbnail: PropTypes.string.isRequired,
+      attibutes: PropTypes.arrayOf(PropTypes.object).isRequired,
+    }).isRequired,
+  }).isRequired,
+};
+
+export default ProductDetails;

90190381dea49c00994b7831ea9d93c4bbb054b0
Elias Carvalho <eliascarvalho01@hotmail.com>
2020-09-29 22:30:43 -0300

    feat: lists the items searched

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,65 @@
 import React, { Component } from 'react';
+import './style/home.css';
+import { getProductsFromCategoryAndQuery } from '../services/api';
+import SearchedItems from '../components/SearchedItems';
 
 class Home extends Component {
+  constructor() {
+    super();
+
+    this.fetchSearchedItem = this.fetchSearchedItem.bind(this);
+    this.onSearchTextChange = this.onSearchTextChange.bind(this);
+
+    this.state = {
+      searchInput: '',
+      spanMessage: 'Digite algum termo de pesquisa ou escolha uma categoria.',
+      searchedItems: undefined,
+    };
+  }
+
+  fetchSearchedItem() {
+    this.setState(
+      async () => {
+        const itemToBeSearched = this.state.searchInput;
+        const searchResult = await getProductsFromCategoryAndQuery('', itemToBeSearched);
+
+        if (searchResult.results.length !== 0) {
+          return this.setState({
+            searchedItems: searchResult.results,
+            searchInput: '',
+            spanMessage: '',
+          });
+        }
+
+        return this.setState({
+          searchedItems: undefined,
+          spanMessage: 'Nenhum produto foi encontrado',
+        });
+      }
+    )
+  }
+
+  onSearchTextChange({ target }) {
+    const { name, value } = target;
+    this.setState({ [name]: value });
+  }
+
   render() {
+    const { searchedItems, spanMessage } = this.state;
+
     return (
-      <div>
-        <input type="text" />
-        <span data-testid="home-initial-message">
-          Digite algum termo de pesquisa ou escolha uma categoria.
-        </span>
+      <div className="search-container">
+        <input
+          name="searchInput" className="search-input" type="text"
+          data-testid="query-input" onChange={this.onSearchTextChange}
+        />
+
+        <button data-testid="query-button" onClick={this.fetchSearchedItem}>Search</button>
+
+        {searchedItems === undefined ?
+          <span data-testid="home-initial-message">{spanMessage}</span> :
+          searchedItems.map((item) => <SearchedItems key={item.id} item={item} />)
+        }
       </div>
     );
   }

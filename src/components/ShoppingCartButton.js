036ba2f7d012029a9d3450509c0947493f4d4cba
Renan Cunha <renan2610@gmail.com>
2020-09-30 00:52:57 -0300

    Simplifying ShoppingCartButton component

--- src/components/ShoppingCartButton.js
+++ src/components/ShoppingCartButton.js
@@ -1,32 +1,13 @@
 import React, { Component } from 'react';
-import { Redirect } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 class ShoppingCartButton extends Component {
-  constructor() {
-    super();
-    this.state = {
-      isRedirectingToCart: false,
-    };
-
-    this.redirectToShoppingCart = this.redirectToShoppingCart.bind(this);
-  }
-
-  redirectToShoppingCart() {
-    this.setState({ isRedirectingToCart: true });
-  }
-
   render() {
-    const { isRedirectingToCart } = this.state;
-    if (isRedirectingToCart) return <Redirect to="/shopping-cart" />;
     return (
       <div>
-        <button
-          type="button"
-          data-testid="shopping-cart-button"
-          onClick={ this.redirectToShoppingCart }
-        >
-          Carrinho
-        </button>
+        <Link to="/shopping-cart">
+          <button type="button" data-testid="shopping-cart-button">Carrinho</button>
+        </Link>
       </div>
     );
   }

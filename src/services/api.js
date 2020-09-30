c09ce08473a8f93412f53bbfd5d26a60105b5116
Aloizio Borges Santos Coelho <aloiziobsc@gmail.com>
2020-09-30 09:18:53 -0300

    rodei o npm run lint, agora deve ir...

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ export async function getCategories() {
   const urlEndPoint = 'https://api.mercadolibre.com/sites/MLB/categories';
   const request = fetch(urlEndPoint)
     .then((response) => response.json())
-    .catch((error) => { console.log(error) });
+    .catch((error) => { console.log(error); });
   return request;
 }
 
@@ -10,6 +10,6 @@ export async function getProductsFromCategoryAndQuery(categoryId, query) {
   const urlEndPoint = `https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}q=${query}`;
   const request = fetch(urlEndPoint)
     .then((response) => response.json())
-    .catch((error) => { console.log(error) });
+    .catch((error) => { console.log(error); });
   return request;
 }

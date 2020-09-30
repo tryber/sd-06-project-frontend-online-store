a7f3e9fd67528668143cb5a1aee1325f33e4f25a
Vinicius Buzato <viniciusbuzato@gmail.com>
2020-09-29 21:07:49 -0300

    Requisito 4 - Concluído

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import { BrowserRouter as Router } from 'react-router-dom';
 import Routes from './services/Routes';
+import Categories from './components/Categories';
 
 
 function App() {
   return (
     <Router>
       <Routes />
+      <Categories />
     </Router>
   );
 }

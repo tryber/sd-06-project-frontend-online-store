ecf0e52f8da0d840a85155c11a18bec8ddad14d9
LughWalle <civalfran1@gmail.com>
2020-09-30 17:00:28 -0300

    create categories

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,11 @@
 export async function getCategories() {
-  // Implemente aquii
+  const response = await fetch('https://api.mercadolibre.com/sites/MLB/categories');
+  const obj = await response.json();
+  return obj;
 }
 
-export async function getProductsFromCategoryAndQuery(/* categoryId, query */) {
-  // Implemente aqui! Quando o fizer, descomente os parâmetros que essa função recebe
+export async function getProductsFromCategoryAndQuery(categoryId, query) {
+  const answer = await fetch(`https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}&q=${query}`);
+  const objson = await answer.json();
+  return objson;
 }

a9d1417a66d770c1fe6b5f7db078dce34946f506
Vinicius Buzato <viniciusbuzato@gmail.com>
2020-09-30 11:33:02 -0300

    Home corrigida

--- src/services/Routes.jsx
+++ src/services/Routes.jsx
@@ -1,14 +1,13 @@
 import React from 'react';
 import { Switch, Route } from 'react-router';
-
-import Header from '../pages/Header';
+import Home from '../pages/Home';
 import Cart from '../pages/Cart';
 
 class Routes extends React.Component {
   render() {
     return (
       <Switch>
-        <Route exact path="/" component={ Header } />
+        <Route exact path="/" component={ Home } />
         <Route path="/cart" component={ Cart } />
       </Switch>
     );

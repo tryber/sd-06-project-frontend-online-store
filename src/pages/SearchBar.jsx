8bc10de3c98392c6233a1b8e3c55834c3641a5b0
Zeca Costa <jccostaso@gmail.com>
2020-09-30 17:03:58 -0300

    feat: add SearchBar.jsx for requirement 4

--- src/pages/SearchBar.jsx
+++ src/pages/SearchBar.jsx
@@ -1,14 +1,42 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-function SearchBar() {
-  return (
-    <div>
-      <label htmlFor="input-text" data-testid="home-initial-message">
-        <input type="text" name="input-text" />
-        Digite algum termo de pesquisa ou escolha uma categoria.
-      </label>
-    </div>
-  );
+import * as api from '../services/api';
+
+class SearchBar extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      categories: [],
+    };
+  }
+
+  componentDidMount() {
+    api.getCategories()
+      .then((categories) => {
+        this.setState({
+          categories,
+        });
+      });
+  }
+
+  render() {
+    const { categories } = this.state;
+    return (
+      <div>
+        <label htmlFor="input-text" data-testid="home-initial-message">
+          <input type="text" name="input-text" />
+          Digite algum termo de pesquisa ou escolha uma categoria.
+        </label>
+        <ul>
+          {categories.map((category) => (
+            <li data-testid="category" key={ category.id }>
+              {category.name}
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
 }
-
 export default SearchBar;

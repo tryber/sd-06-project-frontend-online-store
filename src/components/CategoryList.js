b6191b57ad00c05ce8b07af6ac885c758b5b267a
Cyrano webdev <cyrano@cyranowebdev.com>
2020-09-30 19:20:02 -0300

    Requisito 6: avancando no handler das categorias

--- src/components/CategoryList.js
+++ src/components/CategoryList.js
@@ -2,6 +2,20 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 class CategoryList extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      selectedCategoryId: '',
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange({ target }) {
+    this.setState({ selectedCategoryId: target.id });
+  }
+
   render() {
     const { categories } = this.props;
     const empty = 0;
@@ -11,7 +25,12 @@ class CategoryList extends React.Component {
       {(categories.length > empty)
         ? categories
           .map((category) => (
-            <p data-testid="category" key={ category.id }>{category.name}</p>
+            <label htmlFor={category.id} key={ category.id }>
+              <input
+                type="radio" name="category" id={category.id} data-testid="category"
+                onChange={this.handleChange}
+              />
+            {category.name}</label>
           ))
         : <span>Loading...</span>}
       </section>

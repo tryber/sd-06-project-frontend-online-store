6a451411820fb77b04bd00dcbbef77d604b0850b
Aloizio Borges Santos Coelho <aloiziobsc@gmail.com>
2020-09-30 18:19:47 -0300

    teste local passando faltando eslint

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import './App.css';
 import Home from './pages/Home';
+import Cart from './pages/Cart';
 
 function App() {
   return (
---
+++ src/components/Categories.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import * as api from '../services/api';
+class Categories extends React.Component {
+    constructor() {
+        super()
+        this.fetchCategories = this.fetchCategories.bind(this);
+        this.state = {
+            categories: [],
+        }
+    }
+    componentDidMount() {
+        this.fetchCategories();
+    }
+    async fetchCategories(){
+        this.setState({
+            categories: await api.getCategories()
+        })
+    }
+    render () {
+        const { categories } = this.state
+        return (
+            <div>
+                { categories.map((catego) => (
+                <li data-testid="category" key={catego.id}>
+                { catego.name }
+                </li>
+                )) } 
+            </div>
+        );
+    }
+}
+export default Categories;

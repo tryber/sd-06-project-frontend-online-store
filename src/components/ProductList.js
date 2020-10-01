d4f0fdea55164a91d802f0324c2fba8810be6d7d
Matheus Coutinho <matheuscavalcante88@hotmail.com>
2020-10-01 19:49:41 -0300

    refactoring SearchEngine and Home, create ProductList

---
+++ src/components/ProductList.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+import FoundProducts from './FoundProducts';
+
+export default class ProductList extends Component {
+  render() {
+    const { queryInput, foundItems, products } = this.props;
+    const zero = 0;
+    return (
+      <div>
+        {/* if there is not anything in query input, render this block */}
+        {queryInput === '' && (
+          <p data-testid="home-initial-message">
+            Digite algum termo de pesquisa ou escolha uma categoria.
+          </p>
+        )}
+        {/* if there are no product found from API, render this block */}
+        {queryInput !== '' && !foundItems && (
+          <p data-testid="home-initial-message">
+            Nenhum produto foi encontrado
+          </p>
+        )}
+        {/* if there are products, render this block */}
+        {Object.keys(products).length !== zero &&
+          products.results.map((prod) => (
+            <FoundProducts key={ prod.title } product={ prod } />
+          ))}
+      </div>
+    );
+  }
+}
+
+ProductList.defaultProps = {
+  foundItems: false,
+};
+
+ProductList.propTypes = {
+  queryInput: PropTypes.string.isRequired,
+  foundItems: PropTypes.bool, 
+  products: PropTypes.arrayOf(PropTypes.object).isRequired,
+};
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,17 +1,71 @@
 import React, { Component } from 'react';
 
 import ListCategory from '../../components/ListCategory';
+import ProductList from '../../components/ProductList';
 
 import SearchEngine from '../../components/SearchEngine';
 
 import './styles.css';
 
+import * as api from '../../services/api';
+
 class Home extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      foundItems: false,
+      queryInput: '',
+      categoryInput: '',
+      products: {},
+    };
+
+    this.handleClick = this.handleClick.bind(this);
+    this.handleChangeInput = this.handleChangeInput.bind(this);
+  }
+
+  handleChangeInput(query) {
+    this.setState({ queryInput: query });
+  }
+
+  async handleClick(searchEng) {
+    const { queryInput } = searchEng;
+    const { categoryInput } = this.state;
+    this.setState({ foundItems: true }, async () => {
+      // Fetch products from API
+      const productsFound = await api.getProductsFromCategoryAndQuery(
+        categoryInput,
+        queryInput
+      );
+
+      // set foundItems to false, if there are no products found
+      if (Object.keys(productsFound).length < 1) {
+        this.setState({
+          foundItems: false,
+          products: {},
+        });
+      } else {
+        this.setState({
+          products: productsFound,
+        });
+      }
+    });
+  }
+
   render() {
+    const { queryInput, foundItems, products } = this.state;
     return (
       <div className="home-page">
         <ListCategory />
-        <SearchEngine />
+        <SearchEngine
+          sendQueryInputToHome={ this.handleChangeInput }
+          onClick={ this.handleClick }
+        />
+        <ProductList
+          queryInput={ queryInput }
+          foundItems={ foundItems }
+          products={ products }
+        />
       </div>
     );
   }

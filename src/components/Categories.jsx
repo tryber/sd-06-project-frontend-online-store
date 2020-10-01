3f2fef51bbbe5794f1460e53888e5c664f96d1e9
Artur Forna <fornagieri.artur@gmail.com>
2020-10-01 17:58:22 -0300

    Todos a bordo do trem da dor :rage:

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -2,31 +2,48 @@ import React, { Component } from 'react';
 import * as Cat from '../services/api';
 
 class Categories extends Component {
-    constructor() {
-				super();
-				this.fetchCategories = this.fetchCategories.bind(this);
-        this.state = {
-            categories: [],
-        }
-		}
-		componentDidMount() {
-			this.fetchCategories()
-		}
+	constructor() {
+		super();
+		
+		this.fetchCategories = this.fetchCategories.bind(this);
+		this.getStatusFromCategory = this.getStatusFromCategory.bind(this);
 
-		async fetchCategories() {
-			const result = await Cat.getCategories();
-			this.setState({
-				categories: result,
-			})
-			
+		this.state = {
+				categories: [],
+				categoryObjList: [],
 		}
-    render() {
-        return (
-            <div>
-                {this.state.categories.map((cat) => <div data-testid="category" key={cat.id}>{cat.name}</div>)}
-            </div>
-        );
-    }
+	}
+
+	componentDidMount() {
+		this.fetchCategories()
+	}
+
+	async fetchCategories() {
+		const result = await Cat.getCategories();
+		this.setState({
+			categories: result,
+		})
+	}
+
+	getStatusFromCategory({target}) {
+		this.setState({
+			categoryObjList: [...this.state.categoryObjList, {checkStatus: target.checked, categoryID: target.id}]
+		})
+	}
+
+
+	render() {
+		return (
+			<div>
+				{this.state.categories.map((cat) => 
+				<div>
+					<label data-testid="category" key={cat.id}>
+						<input type="checkbox" id={cat.id} onChange={this.getStatusFromCategory}/> {cat.name}
+					</label>
+				</div>
+				)}
+			</div>
+		)}
 }
 
-export default Categories;
+export default Categories

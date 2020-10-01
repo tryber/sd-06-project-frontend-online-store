5d1b8e50445b76b5a412b5fbfe9c49a3f8e5f05b
Ana Flávia <anaflvia@gmail.com>
2020-10-01 20:09:32 -0300

    Começando requisito 11

--- src/pages/ProductDetails.js
+++ src/pages/ProductDetails.js
@@ -3,6 +3,19 @@ import PropTypes from 'prop-types';
 import CartBtn from '../services/CartBtn';
 
 class ProductDetails extends React.Component {
+  saveEvaluation() {
+    const email = document.getElementById('email').value;
+    const rating = document.getElementById('rating').value;
+    const message = document.getElementById('message').value;
+    const evaluation = {
+      email,
+      rating,
+      message,
+    };
+    const evaluationString = JSON.stringify(evaluation);
+    localStorage.setItem('evaluations', [evaluationString]);
+  }
+
   render() {
     const { location: { state: { title, thumbnail, attibutes } } } = this.props;
     return (
@@ -19,6 +32,27 @@ class ProductDetails extends React.Component {
         <div>
           <CartBtn />
         </div>
+        <form>
+          <input type="text" placeholder="Coloque seu email" id="email" required />
+          <br />
+          <label htmlFor="rating">
+            Avaliação:
+            <input type="number" min={ 0 } max={ 5 } step={ 1 } id="rating" required />
+            <br />
+          </label>
+          <textarea
+            id="message"
+            data-testeid="product-detail-evaluation"
+            placeholder="Mensagem(opcional)"
+          />
+          <br />
+          <button
+            type="button"
+            onClick={ this.saveEvaluation }
+          >
+            Submeter avaliação
+          </button>
+        </form>
       </div>
     );
   }

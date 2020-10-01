434aea60fab53fb31c0f9cedda168ced66554263
Rick Sousa <rickssti@outlook.com>
2020-10-01 19:46:40 -0300

    :sparkles: Created product detail page
    created product detail page and component

--- src/Components/Product.js
+++ src/Components/Product.js
@@ -1,28 +1,41 @@
 import React, { Component } from 'react';
 import propTypes from 'prop-types';
+import { Link } from 'react-router-dom/cjs/react-router-dom.min';
 
 class Product extends Component {
   render() {
-    const { product } = this.props;
-    const { title, thumbnail, price } = product;
+    const { product, search } = this.props;
+    const { title, thumbnail, price, id } = product;
     return (
-      <div>
-        <div data-testid="product">
-          <h5>{ title }</h5>
-          <img src={ thumbnail } alt="fotografia do produto" />
-          <span>{`R$: ${price}`}</span>
+      <Link
+        to={ {
+          pathname: `/product-detail/${id}`,
+          state: {
+            busca: search,
+          },
+        } }
+      >
+        <div data-testid="product-detail-link">
+          <div data-testid="product">
+            <h5>{ title }</h5>
+            <img src={ thumbnail } alt="fotografia do produto" />
+            <span>{`R$: ${price}`}</span>
+          </div>
         </div>
-      </div>
+      </Link>
     );
   }
 }
 
 Product.propTypes = {
+  search: propTypes.instanceOf(Array).isRequired,
   product: propTypes.shape({
     title: propTypes.string,
     thumbnail: propTypes.string,
     price: propTypes.number,
+    id: propTypes.string,
   }).isRequired,
+
 };
 
 export default Product;

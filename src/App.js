8f2c84bc458de3d364b1207f6fe9369aec773752
HCaetano <hrcgeo@gmail.com>
2020-10-01 16:00:36 -0300

    fix(css imports): fix css file paths on App and index files

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { Home } from './pages';
-import './App.css';
+import './styles/App.css';
 
 function App() {
   return (

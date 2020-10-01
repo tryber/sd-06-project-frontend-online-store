68b07f8d7821a2944fa7b4bb0f1b18bf58066338
Matheuscara <matheuscara2000@gmail.com>
2020-10-01 11:19:43 -0300

    Requeriment 7 complete

--- src/pages/product.js
+++ src/pages/product.js
@@ -2,17 +2,17 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import * as api from '../services/api';
 
-export default class product extends Component {
+export default class Product extends Component {
   constructor(props) {
     super(props);
     const { match } = this.props;
     const { id, category } = match.params;
     this.searchApiProducts = this.searchApiProducts.bind(this);
     this.state = {
+      product: [],
       id,
       category,
-      product: [],
-      attributes:[]
+      // attributes: [],
     };
   }
 
@@ -24,30 +24,30 @@ export default class product extends Component {
     const { category, id } = this.state;
     await api
       .getProductsFromCategoryAndQuery(category)
-      .then((res) => {
-        return res.results.filter((element) => element.id === id);
-      })
+      .then((res) => res.results.filter((element) => element.id === id))
       .then((res) => {
         this.setState({
           product: res[0],
-          attributes: res[0].attributes,
+          // attributes: res[0].attributes,
         });
       });
   }
 
   render() {
-    const { product, attributes } = this.state;
-    console.log(product);
+    const { product } = this.state;
     return (
       <div>
-        <h1 data-testid="product-detail-name">{product.title}</h1>
-        <img src={product.thumbnail} alt={product.title} />
-        <p>R$: {product.price}</p>
+        <h1 data-testid="product-detail-name">{ product.title }</h1>
+        <img src={ product.thumbnail } alt={ product.title } />
+        <p>
+          R$:
+          {product.price}
+        </p>
       </div>
     );
   }
 }
 
-product.propTypes = {
+Product.propTypes = {
   match: PropTypes.objectOf.isRequired,
 };

ce96280db8dc539cf84d77797b28d9d80619b526
HCaetano <hrcgeo@gmail.com>
2020-10-01 16:25:51 -0300

    feat(ProductCard): add style, change divs to p to make it more semantic

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
+import '../styles/ProductCard.css';
 
 export default class ProductCard extends React.Component {
   render() {
     const { title, price, thumbnail } = this.props.product;
     return (
       <article data-testid="product">
-        <div>{title}</div>
-        <div>{price}</div>
+        <p>{title}</p>
         <img src={thumbnail} alt={title}/>
+        <p>R$ {price}</p>
       </article>
     );
   }

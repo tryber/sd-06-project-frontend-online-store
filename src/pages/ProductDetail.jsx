160604d42c8b062e824fce2e2c1ed8218a1a65ee
alcantara2509 <alcantara2509@gmail.com>
2020-10-01 15:16:05 -0300

    #9: add products in cart from detail

--- src/pages/ProductDetail.jsx
+++ src/pages/ProductDetail.jsx
@@ -3,10 +3,43 @@ import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
 class ProductDetail extends Component {
+  constructor() {
+    super();
+    this.handleProductQuantityAltering = this.handleProductQuantityAltering.bind(this);
+    this.addItemToCart = this.addItemToCart.bind(this);
+
+    this.state = {
+      quantity: 1,
+    };
+  }
+
+  addItemToCart(product, id, quantity) {
+    const cartItems = JSON.parse(localStorage.getItem('cartItems')) || [];
+    const itemAlreadyInCart = cartItems.findIndex(({ product: item }) => item.id === id);
+    if (cartItems[itemAlreadyInCart]) {
+      cartItems[itemAlreadyInCart].quantity += quantity;
+    } else {
+      cartItems.push({ product, quantity });
+    }
+    localStorage.setItem('cartItems', JSON.stringify(cartItems));
+  }
+
+  handleProductQuantityAltering({ target }) {
+    let { quantity } = this.state;
+    const { name } = target;
+    if (name === 'plus') {
+      quantity += 1;
+    } else {
+      quantity = quantity === 1 ? 1 : quantity - 1;
+    }
+    this.setState({ quantity });
+  }
+
   render() {
     const { products } = this.props.location.state;
     const { id } = this.props.match.params;
     const product = products.find((productItem) => id === productItem.id);
+    const { quantity } = this.state;
     return (
       <>
         <Link to="/shopping-cart" data-testid="shopping-cart-button">cart</Link>
@@ -19,10 +52,33 @@ class ProductDetail extends Component {
           </span>
           <img src={ product.thumbnail } alt="imagem do produto" />
           <div>
-            <p>especificações Tecnicas</p>
-            <ul>
-              <li>Cada especificação</li>
-            </ul>
+            <p>Adicionar ao carrinho</p>
+            <div>
+              <button
+                type="button"
+                onClick={ this.handleProductQuantityAltering }
+                name="minus"
+              >
+                -
+
+              </button>
+              <input type="number" min={ 1 } step={ 1 } value={ quantity } />
+              <button
+                type="button"
+                onClick={ this.handleProductQuantityAltering }
+                name="plus"
+              >
+                +
+
+              </button>
+              <button
+                type="button"
+                data-testid="product-detail-add-to-cart"
+                onClick={ () => this.addItemToCart(product, id, quantity) }
+              >
+                Adicionar
+              </button>
+            </div>
           </div>
         </div>
       </>

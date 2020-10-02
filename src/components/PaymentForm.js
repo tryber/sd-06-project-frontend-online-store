cda62a1cba4eae6b3f055bb24de641fc09daf093
Paulo <paulocheloni@gmail.com>
2020-10-02 19:20:43 -0300

    adding state paymentForm

--- src/components/PaymentForm.js
+++ src/components/PaymentForm.js
@@ -1,6 +1,19 @@
 import React from 'react';
 
 class PaymentForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      fullName: '',
+      email: '',
+      cpf: '',
+      email: '',
+      phone: '',
+      cep: '',
+      adress: '',
+    };
+  }
+
   render() {
     return (
       <div>

1a5f25907600d27514da5b0c184459268528e6bb
Cyrano webdev <cyrano@cyranowebdev.com>
2020-10-02 19:29:53 -0300

    req12 - elementos forms PaymentForm

--- src/components/PaymentForm.js
+++ src/components/PaymentForm.js
@@ -17,13 +17,29 @@ class PaymentForm extends React.Component {
   render() {
     return (
       <form>
-        <label htmlFor="nome-completo">
+        <label htmlFor="fullName">
           Nome completo
-          <input type="text" data-testid="checkout-fullname" id="nome-completo" />
+          <input type="text" data-testid="checkout-fullname" id="fullName" />
         </label>
         <label htmlFor="email">
-          Nome completo
+          E-mail
           <input type="text" data-testid="checkout-email" id="email" />
+        </label>
+        <label htmlFor="cpf">
+          CPF
+          <input type="number" data-testid="checkout-cpf" id="cpf" maxLength="11" />
+        </label>
+        <label htmlFor="phone">
+          Telefone
+          <input type="number" data-testid="checkout-phone" id="phone" maxLength="11" />
+        </label>
+        <label htmlFor="cep">
+          CEP
+          <input type="number" data-testid="checkout-cep" id="cep" maxLength="11" />
+        </label>
+        <label htmlFor="address">
+          Endereço
+          <input type="text" data-testid="checkout-address" id="address" maxLength="11" />
         </label>
       </form>
     );

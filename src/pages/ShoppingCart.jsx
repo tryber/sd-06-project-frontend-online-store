e0067eaff955da01cdaa090b3b3ae3e2cc933e1a
HCaetano <hrcgeo@gmail.com>
2020-10-02 18:23:25 -0300

    refactor(ShoppingCart): minimal styling refactors to adhere to linter policy

--- src/pages/ShoppingCart.jsx
+++ src/pages/ShoppingCart.jsx
@@ -1,17 +1,22 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import '../styles/ShoppingCart.css';
-import cart from './../img/cart.png';
-import goHome from './../img/back.png';
-import empytCart from './../img/empty-cart.png';
+import cart from '../img/cart.png';
+import goHome from '../img/back.png';
+import empytCart from '../img/empty-cart.png';
 
 class ShoppingCart extends Component {
   render() {
     return (
       <div>
         <div className="icon-box">
-          <Link to="/"><img src={ goHome } alt="imagem voltar para home"   /></Link>
-          <Link to="/cart"><img src={ cart } alt="imagem do carrinho" /><span>Carrinho de Compras</span></Link>
+          <Link to="/">
+            <img src={ goHome } alt="imagem voltar para home" />
+          </Link>
+          <Link to="/cart">
+            <img src={ cart } alt="imagem do carrinho" />
+            <span>Carrinho de Compras</span>
+          </Link>
         </div>
         <div className="empty-cart">
           <img src={ empytCart } alt="carrinho vazio" />

31aaf8e6835f0f2639cf29973e37dfc80b61026d
luiz <phellipe501@gmail.com>
2020-10-02 13:38:55 -0300

    Grupo 25 - Requistito 1 ao 9 - Apenas 5 falhando Linter

--- src/App.js
+++ src/App.js
@@ -7,30 +7,40 @@ import ProductDetails from './pages/ProductDetails';
 
 class App extends React.Component {
   constructor() {
-    super()
+    super();
     this.cartAdd = this.cartAdd.bind(this);
     this.state = {
       cart: [],
-    }
+    };
   }
 
   cartAdd(product) {
-    console.log(product)
-    this.setState({cart: [...this.state.cart, product]})
-    return this.state.cart.length
+    const { cart } = this.state;
+    this.setState(({ cart: [...cart, product] }));
+    return cart.length;
   }
 
   render() {
+    const { cart } = this.state;
     return (
       <BrowserRouter>
         <Switch>
-          <Route path="/cart" render={ (props) => <CartPage {...props} cartItems={this.state.cart}/>} />
-          <Route path="/details/:id" render={ (props) => <ProductDetails {...props} cartAdd={this.cartAdd} /> } />
-          <Route path="/" render={ (props) => <HomePage {...props} cartAdd={this.cartAdd} /> } />
+          <Route
+            path="/cart"
+            render={ (props) => <CartPage { ...props } cartItems={ cart } /> }
+          />
+          <Route
+            path="/details/:id"
+            render={ (props) => <ProductDetails { ...props } cartAdd={ this.cartAdd } /> }
+          />
+          <Route
+            path="/"
+            render={ (props) => <HomePage { ...props } cartAdd={ this.cartAdd } /> }
+          />
         </Switch>
       </BrowserRouter>
     );
-  } 
+  }
 }
 
 export default App;

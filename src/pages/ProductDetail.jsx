e7563833645904c572f0fc30093d42c2a94e0b12
Dilenio Enderle <dilenio@gmail.com>
2020-10-02 10:14:36 -0300

    feat: Create a details page to show specific product

---
+++ src/pages/ProductDetail.jsx
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+export default class ProductDetail extends Component {
+  render() {
+    console.log(this.props)
+    const { title, price, thumbnail, available_quantity } = this.props.location.product;
+    return (
+      <div>
+        <div>
+          <Link to="/">Voltar</Link>
+        </div>
+        <div className="product-details">
+          <div className="product-main">
+            <h4 data-testid="product-detail-name">{ title } - R${ price }</h4>
+            <img src={ thumbnail } alt="Thumbnail" />
+          </div>
+          <div>
+            <h4>Especificações técnicas</h4>
+            <p>Quantidade disponível: { available_quantity }</p>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}

40c00cca693bba8ab3aee4ba603860b52fddf508
Clayton Miguel Da Silva <claytonmiguel1805@gmail.com>
2020-10-02 20:16:35 -0300

    req 10 funcionando

--- src/pages/CartPage.jsx
+++ src/pages/CartPage.jsx
@@ -2,9 +2,28 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import { cart } from '../dados/cart_arrayProductList';
 
-class CartPage extends Component {  
+class CartPage extends Component {
+  constructor() {
+    super();
+    this.handleClayton = this.handleClayton.bind(this);
+    this.state = {
+      quantity: 1,
+    };
+  }
+
+  handleClayton({ target }) {
+    const id = target.name;
+    const value = target.value === '+' ? 1 : -1;
+    // console.log(value);
+    // console.log(target.value);
+    if (!this.state[id]) {
+      this.setState({ [id]: 1 + value });
+    } else {
+      this.setState((stateAntigo) => ({ [id]: stateAntigo[id] + value }));
+    }
+  }
+
   render() {
-    // console.log(cart);
 
     return (
       <div>
@@ -13,20 +32,33 @@ class CartPage extends Component {
         {
           (cart <= 0 )
           ? <p data-testid="shopping-cart-empty-message">Seu carrinho está vazio</p>
-          : cart.map(product => (
-            <div key={product.id}>
-              <img src={product.thumbnail}/>
-              <h2 data-testid="shopping-cart-product-name" >{product.title}</h2>
-              <div className="quantidade" >
-                <button > - </button>
-                <p data-testid="shopping-cart-product-quantity"> 1 </p>
-                <button > + </button>
-              </div>
-              <p>R$ {product.price}</p>
-            </div>
-          )
-          )
-
+          : cart.map((product) => {
+              const { id, thumbnail, title, price } = product;
+              return (
+                <div key={id}>
+                  <img src={thumbnail}/>
+                  <h2 data-testid="shopping-cart-product-name">{title}</h2>
+                  <div className="quantidade">
+                    <button
+                      value="-"
+                      name={id}
+                      type="button"
+                      data-testid="product-decrease-quantity"
+                      onClick={this.handleClayton}
+                    > - </button>
+                    <p data-testid="shopping-cart-product-quantity">{this.state[id] ? this.state[id] : 1}</p>
+                    <button
+                      value="+"
+                      name={id}
+                      type="button"
+                      data-testid="product-increase-quantity"
+                      onClick={this.handleClayton}
+                    > + </button>
+                  </div>
+                  <p>{`R$${price}`}</p>
+                </div>
+              );
+            })
         }
       </div>
     );

82ae049aaaee8ce80d6700e0febed132c321405a
Paulo Lins <38190075+paulo-lins@users.noreply.github.com>
2020-10-02 17:17:41 -0300

    Update ProductCard.jsx

--- src/pages/ProductCard.jsx
+++ src/pages/ProductCard.jsx
@@ -2,38 +2,19 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
 export default class ProductCard extends Component {
-  constructor() {
-    super();
-
-    this.state = {
-      shoppingCart: [],
-    }
-  }
-
-  buyingHandler() {
-    this.setState((prevState, props) => {
-      shoppingCart: (...prevState, this.shoppingCart)
-    })
-  }
-
+ 
   render() {
-    const { id, title, price, thumbnail } = this.props.product;
+    const { title, price, thumbnail } = this.props.product;
     return (
       <div data-testid="product" className="product-card">
         <img src={ thumbnail } alt="Thumbnail" />
         <h4>{ title }</h4>
         <h5>{ price }</h5>
-<<<<<<< HEAD
-        <Link to="ShoppingCart">
+        <Link to={ { pathname: `/shoppingcart/`, product: this.props.product } }
+          data-testid="product-add-to-cart">
           ADICIONAR AO CARRINHO
         </Link>
-=======
-        <Link
-          to={ { pathname: `/product/${id}`, product: this.props.product } }
-          data-testid="product-detail-link"
-        >
-        Detalhes</Link>
->>>>>>> 31ebb6c13eff8ddcc2ff0b712b9e05f634a62bed
+
       </div>
     );
   }

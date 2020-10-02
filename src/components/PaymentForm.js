61aa4c795beee05cb055965a4acefb6bd6567464
Cyrano webdev <cyrano@cyranowebdev.com>
2020-10-02 19:43:38 -0300

    req12 - all form elements Payment Forms

--- src/components/PaymentForm.js
+++ src/components/PaymentForm.js
@@ -7,7 +7,6 @@ class PaymentForm extends React.Component {
       fullName: '',
       email: '',
       cpf: '',
-      email: '',
       phone: '',
       cep: '',
       address: '',
@@ -20,31 +19,65 @@ class PaymentForm extends React.Component {
   }
 
   render() {
+    const handler = this.handleChange;
     return (
       <form>
         <label htmlFor="fullName">
           Nome completo
-          <input type="text" data-testid="checkout-fullname" id="fullName" />
+          <input
+            type="text"
+            data-testid="checkout-fullname"
+            id="fullName"
+            onChange={ handler }
+          />
         </label>
         <label htmlFor="email">
           E-mail
-          <input type="text" data-testid="checkout-email" id="email" />
+          <input
+            type="text"
+            data-testid="checkout-email"
+            id="email"
+            onChange={ handler }
+          />
         </label>
         <label htmlFor="cpf">
           CPF
-          <input type="number" data-testid="checkout-cpf" id="cpf" maxLength="11" />
+          <input
+            type="number"
+            data-testid="checkout-cpf"
+            id="cpf"
+            maxLength="11"
+            onChange={ handler }
+          />
         </label>
         <label htmlFor="phone">
           Telefone
-          <input type="number" data-testid="checkout-phone" id="phone" maxLength="11" />
+          <input
+            type="number"
+            data-testid="checkout-phone"
+            id="phone"
+            maxLength="11"
+            onChange={ handler }
+          />
         </label>
         <label htmlFor="cep">
           CEP
-          <input type="number" data-testid="checkout-cep" id="cep" maxLength="11" />
+          <input
+            type="number"
+            data-testid="checkout-cep"
+            id="cep"
+            maxLength="11"
+            onChange={ handler }
+          />
         </label>
         <label htmlFor="address">
           Endereço
-          <input type="text" data-testid="checkout-address" id="address" maxLength="11" />
+          <textarea
+            data-testid="checkout-address"
+            id="address"
+            maxLength="11"
+            onChange={ handler }
+          />
         </label>
       </form>
     );

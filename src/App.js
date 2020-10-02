65bfa5c849ed60fd53318a0c5b5e2d8fa36a64ca
isabellavjs <isabellavjs@gmail.com>
2020-10-02 17:16:16 -0300

    Changing App function to class

--- src/App.js
+++ src/App.js
@@ -4,17 +4,21 @@ import './App.css';
 import Home from './pages/Home';
 import EmptyCart from './pages/EmptyCart';
 import ProductDetails from './pages/ProductDetails';
+import ShoppingCart from './pages/ShoppingCart';
 
-function App() {
-  return (
-    <BrowserRouter>
-      <Switch>
-        <Route path="/EmptyCart" component={ EmptyCart } />
-        <Route exact path="/" component={ Home } />
-        <Route path="/ProductDetails/:id" component={ ProductDetails } />
-      </Switch>
-    </BrowserRouter>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <Switch>
+          <Route path="/EmptyCart" component={ EmptyCart } />
+          <Route exact path="/" component={ Home } />
+          <Route path="/ProductDetails/:id" component={ ProductDetails } />
+          <Route path="/ShoppingCart/:id" component={ ShoppingCart } />
+        </Switch>
+      </BrowserRouter>
+    );
+  }
 }
 
 export default App;
--- src/pages/ShoppingCart.js
+++ src/pages/ShoppingCart.js
@@ -2,10 +2,11 @@ import React from 'react';
 
 
 class ShoppingCart extends React.Component {
-
   render() {
     return (
-
+      <div>
+        Product
+      </div>
     );
   }
 }

c0c08cf7ed7d78c0d30c90f47db73c396fddab72
Dilenio Enderle <dilenio@gmail.com>
2020-10-02 12:24:09 -0300

    fix: remove console.log

--- src/pages/ProductDetail.jsx
+++ src/pages/ProductDetail.jsx
@@ -3,7 +3,6 @@ import { Link } from 'react-router-dom';
 
 export default class ProductDetail extends Component {
   render() {
-    console.log(this.props)
     const { title, price, thumbnail, available_quantity } = this.props.location.product;
     return (
       <div>

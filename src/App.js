050ed10d8604300c5a381a9fcbffb2ce53bf6864
ricardorosa-dev <ricardorosa@gmail.com>
2020-10-04 19:00:15 -0300

    testando.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Home from './Pages/Home'
+import ShoppingCart from './Pages/ShoppingCart'
+import ProductDetails from './Pages/ProductDetails';
 import './App.css';
 
 class App extends React.Component {
@@ -8,7 +10,9 @@ class App extends React.Component {
     return (
       <BrowserRouter>
         <Switch>
-          <Route path="/" component={ Home } />
+          <Route exact path="/" component={ Home } />
+          <Route path="/cart" component={ ShoppingCart } />
+          <Route path="/productDetails" component={ ProductDetails } />
         </Switch>
       </BrowserRouter>
     )
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,5 +1,9 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import * as api from '../services/api';
+import CategoryList from '../Components/CategoryList';
+import ProductList from '../Components/ProductList';
+import '../App.css';
 
 class Home extends React.Component {
   constructor() {
@@ -8,7 +12,13 @@ class Home extends React.Component {
     this.state = {
       allProducts: [],
       allCategories: [],
+      listagem: '',
+      cart: 'Seu carrinho está vazio',
+      search: [],
+      noCarrinho: 'uma frase empolgada',
     }
+
+    this.handleSearch = this.handleSearch.bind(this);
   }
 
   async componentDidMount() {
@@ -27,9 +37,45 @@ class Home extends React.Component {
       })
   }
 
+  async handleSearch(e) {
+    const busca = await api.getProductsFromCategoryAndQuery('', e.target.value)
+    await this.setState({
+      search: busca,
+    });
+    // await console.log(busca);
+  }
+
   render() {
     return (
-      <h2>no</h2>
+      <div className="wrapper">
+        <aside>
+          <CategoryList categories={ this.state.allCategories } />
+        </aside>
+        <main>
+          <input type="text" placeholder="Busca" onChange={ this.handleSearch } />
+          <Link to={ {
+            pathname: '/cart',
+            state: {
+              cart: this.state.cart,
+            }
+          } } >
+            <img src='./tt.jpg' alt="Meu carrinho" />
+          </Link>
+          {/* { this.state.listagem === '' &&
+            this.state.listagem === '' ? (
+              this.setState({
+                valor: 'Digite algum termo de pesquisa ou escolha uma categoria',
+              })
+            ) : (
+              this.setState({
+                valor: 'Digite algum termo de pesquisa ou escolha uma categoria',
+              })
+            )
+          } */}
+          <ProductList search={ this.state.search } noCarrinho={ this.state.noCarrinho } />
+          {/* < p data-testid="home-initial-message">{ this.state.valor }</p> */ }
+        </main>
+      </div >
     )
   }
 }

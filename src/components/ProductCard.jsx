b5064d80c8639e6a7bca49a24747261e5994cc31
HCaetano <hrcgeo@gmail.com>
2020-10-04 12:18:08 -0300

    feat(ProductCard): add button to include clicked product in the shopping cart

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,15 +1,38 @@
 import React from 'react';
 
 export default class ProductCard extends React.Component {
+  constructor(props) {
+    super();
+
+    this.state = {
+      productId: props.product.id,
+    };
+
+    this.onClick = this.onClick.bind(this);
+  }
+
+  onClick() {
+    const callback = this.props.callback;
+
+    callback(this.state.productId);
+  }
+
   render() {
-    const { product } = this.props;
-    const { title, price, thumbnail } = product;
+    const { title, price, thumbnail } = this.props.product;
+
     return (
-      <div data-testid="product">
+      <article data-testid="product">
         <div>{title}</div>
         <div>{price}</div>
-        <img src={thumbnail} alt={`image${title}`}/>
-      </div>
-    )
+        <img src={thumbnail} alt={title}/>
+        <button
+          type="submit"
+          data-testid="product-add-to-cart"
+          onClick={ this.onClick }
+        >
+          Adicionar ao Carrinho
+        </button>
+      </article>
+    );
   }
 }

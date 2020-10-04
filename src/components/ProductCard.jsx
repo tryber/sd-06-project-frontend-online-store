8c4cbfb6bd744781daf044bb3aca16906d2f1d9a
HCaetano <hrcgeo@gmail.com>
2020-10-04 14:15:08 -0300

    refactor(ProductCard): extract Component from React on import to keep class declaration the same way on the project

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-export default class ProductCard extends React.Component {
+export default class ProductCard extends Component {
   constructor(props) {
     super();
 

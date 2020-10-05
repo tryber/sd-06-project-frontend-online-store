54b06391d6321c3eaf67a53ed66171458a451412
higoramorim <1810042@aluno.univesp.br>
2020-10-05 19:40:38 -0300

    requisito11 add funcionalidade

---
+++ src/StarsRating.js
@@ -0,0 +1,36 @@
+import React, { useState } from 'react';
+import { FaStar } from 'react-icons/fa';
+
+const StarsRating = () => {
+  const [rating, setRating] = useState(null);
+  const [hover, setHover] = useState(null);
+  const a = 5;
+  return (
+    <div>
+      {[...Array(a)].map((star, i) => {
+        const ratingValue = i + 1;
+        return (
+          <label
+            key={ `${star}` }
+            htmlFor="rating"
+          >
+            <input
+              type="radio"
+              name="rating"
+              value={ ratingValue }
+              onClick={ () => setRating(ratingValue) }
+            />
+            <FaStar
+              size={ 20 }
+              className="star"
+              color={ ratingValue <= (hover || rating) ? '#FFC107' : '#E4E5E9' }
+              onMouseEnter={ () => setHover(ratingValue) }
+              onMouseLeave={ () => setHover(null) }
+            />
+          </label>
+        );
+      })}
+    </div>
+  );
+};
+export default StarsRating;

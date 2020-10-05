0d52c9ea8f6fbb7bbcf83d6658e45bf7366c542a
Tiago Berwanger <berwangertiago@gmail.com>
2020-10-05 14:58:20 -0300

    Requirement 11: implementing rate, mail input and textarea - static (not saving)

--- src/components/ProductDetails.js
+++ src/components/ProductDetails.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Rater from 'react-rater'
+import 'react-rater/lib/react-rater.css'
 
 class ProductDetails extends React.Component {
   constructor() {
@@ -33,6 +35,27 @@ class ProductDetails extends React.Component {
         <h1 data-testid="product-detail-name">{title}</h1>
         <p>{price}</p>
         <img alt="Product" src={ thumbnail } />
+        <div className="rate-form">
+          <hr />
+          <h2>Avaliações</h2>
+          <form>
+            <Rater total={5} rating={0} />
+            <br />
+            <input 
+              type="text" 
+              placeholder="Email" 
+              required>
+            </input>
+            <br />
+            <textarea 
+              data-testid="product-detail-evaluation" 
+              type="text" 
+              placeholder="Mensagem (opcional)">
+            </textarea>
+            <br />
+            <button>Avaliar</button>
+          </form>
+        </div>
       </div>
     );
   }

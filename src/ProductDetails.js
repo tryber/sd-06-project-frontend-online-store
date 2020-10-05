f0244b20449722e32319c3af5f01db78ca703abc
Marxneves <marxkhristie@gmail.com>
2020-10-05 19:45:29 -0300

    Group-24: Requirement 9 done

--- src/ProductDetails.js
+++ src/ProductDetails.js
@@ -4,27 +4,82 @@ import PropTypes from 'prop-types';
 import shoppingCartImage from './Images/shoppingcart.png';
 
 class ProductDetails extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      cartProducts: [],
+      quantityCartProducts: 0,
+    };
+    this.addCart = this.addCart.bind(this);
+  }
+
+  addCart() {
+    const { location: { state } } = this.props;
+    this.setState((prevState) => (
+      {
+        cartProducts: [state],
+        quantityCartProducts: prevState.quantityCartProducts + 1,
+      }
+    ), () => {
+      if (localStorage.Cart) {
+        const update = JSON.parse(localStorage.Cart);
+        const menosUm = -1;
+        const { cartProducts } = this.state;
+        const indexCart = update
+          .findIndex((item) => item.cartProducts[0].id === cartProducts[0].id);
+        if (indexCart !== menosUm) {
+          const { quantityCartProducts } = this.state;
+          update[indexCart].quantityCartProducts = quantityCartProducts;
+          localStorage.Cart = JSON.stringify(update);
+        } else {
+          localStorage.Cart = JSON.stringify(
+            [...JSON.parse(localStorage.Cart), this.state],
+          );
+        }
+      } else {
+        localStorage.Cart = JSON.stringify([this.state]);
+      }
+    });
+  }
+
   render() {
-    const { location: { state: { title, thumbnail, price } } } = this.props;
+    const { location: { state: { title, thumbnail, price, attributes } } } = this.props;
+    const { quantityCartProducts } = this.state;
     return (
       <div>
         <header className="navigation">
           <Link to="/">Voltar</Link>
-          <Link to="/shoppingcart">
+          <Link
+            data-testid="shopping-cart-button"
+            to={ { pathname: '/shoppingcart', state: this.state } }
+          >
             <img width="30px" src={ shoppingCartImage } alt="Cart" />
+            <span>{ quantityCartProducts }</span>
           </Link>
         </header>
         <div className="main-productdetails">
           <div className="left-details">
-            <p data-testid="product-detail-name">{ `${title} - R$ ${price}` }</p>
+            <p data-testid="product-detail-name">
+              { `${title} - R$ ${price}` }
+            </p>
             <img src={ thumbnail } alt={ title } />
+            <button
+              data-testid="product-detail-add-to-cart"
+              type="button"
+              onClick={ this.addCart }
+            >
+              Adicionar ao carrinho
+            </button>
           </div>
           <div>
             <p>Especificações Técnicas</p>
             <ul>
-              <li>Quantidade disponivel: 0</li>
-              <li>Especificação Técnicas 2</li>
-              <li>Especificação Técnicas 3</li>
+              { attributes.map((attribute) => (
+                <li key={ attribute.id }>
+                  { `${attribute.name}: ${attribute.value_name}` }
+                </li>
+              )) }
             </ul>
           </div>
         </div>
@@ -36,9 +91,11 @@ class ProductDetails extends React.Component {
 ProductDetails.propTypes = {
   location: PropTypes.shape({
     state: PropTypes.shape({
+      id: PropTypes.string.isRequired,
       title: PropTypes.string.isRequired,
       price: PropTypes.number.isRequired,
       thumbnail: PropTypes.string.isRequired,
+      attributes: PropTypes.arrayOf(String).isRequired,
     }).isRequired,
   }).isRequired,
 };
--- src/ShoppingCartPage.jsx
+++ src/ShoppingCartPage.jsx
@@ -1,12 +1,48 @@
 import React from 'react';
-// import { Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import Product from './Product';
 
 class ShoppingCartPage extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      cartItems: [],
+      quantity: [],
+      emptyCart: true,
+    }
+  };
+
+  componentDidMount() {
+    if (localStorage.Cart) {
+      const cartItems = JSON.parse(localStorage.Cart).map(item => item.cartProducts[0]);
+      const quantity = JSON.parse(localStorage.Cart).map(item => item.quantityCartProducts);
+      this.setState({ cartItems: [...cartItems], emptyCart: false, quantity });
+    }
+  }
+
   render() {
+    const { emptyCart, cartItems, quantity } = this.state;
     return (
-      <p data-testid="shopping-cart-empty-message">
-        Seu carrinho está vazio
-      </p>
+      <div>
+        <Link to="/" >Voltar</Link>
+        {(emptyCart)
+          ? < p data-testid="shopping-cart-empty-message" >
+            Seu carrinho está vazio
+            </p >
+          : cartItems.map((product, index) => (
+            <div key={ product.id }>
+              <Product
+                id={ product.id }
+                title={ product.title }
+                image={ product.thumbnail }
+                price={ product.price }
+                product={ product }
+              />)
+              <p>Quantidade: <span data-testid="shopping-cart-product-quantity">{ quantity[index] }</span></p>
+            </div>
+          )) }
+      </div>
     );
   }
 }

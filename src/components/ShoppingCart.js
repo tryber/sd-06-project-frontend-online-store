701afd36b4be844ee76fb856dd9d133b4e24ebf6
Cássio Rubens <cassiorubensadv@gmail.com>
2020-10-05 17:36:47 -0300

    [Group 22] FEAT: requirement 10

--- src/components/ShoppingCart.js
+++ src/components/ShoppingCart.js
@@ -10,6 +10,46 @@ class ShoppingCart extends React.Component {
       items: [...cartItems],
       addedItems,
     };
+
+    this.handleIncreaseQuantity = this.handleIncreaseQuantity.bind(this);
+    this.handleDecreaseQuantity = this.handleDecreaseQuantity.bind(this);
+    this.removeItem = this.removeItem.bind(this);
+  }
+
+  handleIncreaseQuantity(id) {
+    const { addedItems } = this.state;
+    let quantity = addedItems[id];
+    quantity += 1;
+    addedItems[id] = quantity;
+    this.setState({
+      addedItems,
+    });
+  }
+
+  handleDecreaseQuantity(id) {
+    const { addedItems } = this.state;
+    let quantity = addedItems[id];
+    quantity -= 1;
+
+    const zero = 0;
+    if (quantity < zero) {
+      quantity = zero;
+    }
+    addedItems[id] = quantity;
+
+    this.setState({
+      addedItems,
+    });
+  }
+
+  removeItem(id) {
+    const { addedItems, items } = this.state;
+    addedItems[id] = 0;
+    const itemsFiltered = items.filter((item) => item.id !== id);
+    this.setState({
+      addedItems,
+      items: itemsFiltered,
+    });
   }
 
   render() {
@@ -27,13 +67,48 @@ class ShoppingCart extends React.Component {
 
     return (
       items.map((item) => {
-        const { id, title, thumbnail } = item;
+        const { id, title, thumbnail, price } = item;
 
         return (
           <div key={ id }>
             <h3 data-testid="shopping-cart-product-name">{ title }</h3>
             <img alt="Product in the cart" src={ thumbnail } />
-            <p data-testid="shopping-cart-product-quantity">{ addedItems[`${id}`] }</p>
+            <div>
+              <button
+                data-testid="product-increase-quantity"
+                type="button"
+                onClick={ () => this.handleIncreaseQuantity(id) }
+              >
+                {' '}
+                +
+              </button>
+              <p data-testid="shopping-cart-product-quantity">{ addedItems[`${id}`] }</p>
+
+              <button
+                data-testid="product-decrease-quantity"
+                type="button"
+                onClick={ () => this.handleDecreaseQuantity(id) }
+              >
+                {' '}
+                -
+              </button>
+            </div>
+            <div>
+              R$:
+              {' '}
+              { price * addedItems[`${id}`] }
+            </div>
+            <button
+              type="button"
+              onClick={ () => this.removeItem(id) }
+            >
+              {' '}
+              X
+            </button>
+            <span>
+              Valor Total da Compra R$:
+            </span>
+            <button type="button">Finalizar Compra</button>
           </div>
         );
       })

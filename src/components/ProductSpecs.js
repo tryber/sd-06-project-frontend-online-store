99c81907c7de1ea23180832a63b5ac31425c89f1
Renan Cunha <renan2610@gmail.com>
2020-10-05 15:43:40 -0300

    Creating ProductSpecs component

---
+++ src/components/ProductSpecs.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class ProductSpecs extends Component {
+  render() {
+    return (
+      <div>
+        TESTE
+      </div>
+    )
+  }
+}
+
+export default ProductSpecs;

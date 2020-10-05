f314826c3404a49474ed323a3722578eea7b57ec
Matheus Coutinho <matheuscavalcante88@hotmail.com>
2020-10-05 18:26:29 -0300

    add to cart button

--- src/pages/ProductDetails.js
+++ src/pages/ProductDetails.js
@@ -1,21 +1,45 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
 import PropTypes from 'prop-types';
 
 import ShoppingCartButton from '../components/ShoppingCartButton';
 
+import './styles/ProductDetails.css';
+
 class ProductDetails extends Component {
   render() {
-    const { location: { state: {
-      title,
-      thumbnail,
-      price } } } = this.props;
+    const {
+      location: {
+        state: { title, thumbnail, price },
+      },
+      match: {
+        params: { productId },
+      },
+    } = this.props;
     return (
-      <div data-testid="product-detail-name">
-        <h1>{title}</h1>
-        <img src={ thumbnail } alt={ `Detalhes do Produto: ${title}` } />
-        <span>{`R$ ${price}` }</span>
-        <p>{}</p>
-        <ShoppingCartButton />
+      <div>
+        <div className="product-detail-header">
+          <Link to="/">Voltar</Link>
+          <ShoppingCartButton />
+        </div>
+        <div data-testid="product-detail-name" className="product-detail">
+          <h1>{title}</h1>
+          <img src={ thumbnail } alt={ `Detalhes do Produto: ${title}` } />
+          <span>{`R$ ${price}`}</span>
+          <p>{}</p>
+        </div>
+        <div className="action">
+          <Link
+            to={ {
+              pathname: '/shopping-cart',
+              productId,
+            } }
+            data-testid="product-detail-add-to-cart"
+          >
+            Adicionar ao carrinho
+          </Link>
+        </div>
       </div>
     );
   }
@@ -27,6 +51,11 @@ ProductDetails.propTypes = {
       title: PropTypes.string,
       thumbnail: PropTypes.string,
       price: PropTypes.number,
+    }),
+  }).isRequired,
+  match: PropTypes.shape({
+    params: PropTypes.shape({
+      productId: PropTypes.string,
     }),
   }).isRequired,
 };

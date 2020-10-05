fec93eb3b64cb7f89549cff801f3f7620ec2cdce
Mariano Ariel Diaz Elsztain <made@poli.ufrj.br>
2020-10-05 15:50:10 -0300

    testing cart button with Link, imported properly

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 import ProductList from './ProductList';
 import * as api from '../services/api';
 import imgCart from '../img/imgCart.jpg';
 import CategoryList from './CategoryList';
-import { Link } from 'react-router-dom';
 
 class Home extends React.Component {
   constructor(props) {

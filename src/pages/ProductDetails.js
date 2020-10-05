939232531f7432e4a4adeda78b2c48ff1f3e3935
Daniel Menescal <danielmjales@bct.ect.ufrn.br>
2020-10-05 16:49:25 -0300

    passing info to ProductDetails

--- src/pages/ProductDetails.js
+++ src/pages/ProductDetails.js
@@ -3,9 +3,18 @@ import { Link } from 'react-router-dom';
 
 class ProductDetails extends Component {
   render() {
+    const { title, thumbnail, price } = this.props.location.state.product;
     return (
       <div>
-        <p>Oi</p>
+        <h2 data-testid="product-detail-name">{ `${title} - ${price}$` }</h2>
+        <img src={ thumbnail } alt="product" />
+        <h3>Especificações Técnicas</h3>
+        <ul>
+          <li>Especificacoes</li>
+          <li>Especificacoes</li>
+          <li>Especificacoes</li>
+          <li>Especificacoes</li>
+        </ul>
       </div>
     );
   }

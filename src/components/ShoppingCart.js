fe80915887fa1411a4c553f9fa56431a0e44f08b
AlexanderFeitosa <alexander.feitosa@gmail.com>
2020-10-05 19:11:52 -0300

    Starting Requirement 9

--- src/components/ShoppingCart.js
+++ src/components/ShoppingCart.js
@@ -1,11 +1,28 @@
 import React, { Component } from 'react';
 
 class ShoppingCart extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      productList: this.props.cartList,
+    };
+  }
+  
   render() {
+    const { productList } = this.state;
+    if (productList.length === 0)
     return (
       <span data-testid="shopping-cart-empty-message">Seu carrinho está vazio</span>
+    );
+
+    return (
+      <div>
+        <span>Empty List</span>
+      </div>
     );
   }
 }
 
 export default ShoppingCart;
+export { ShoppingCart as addItem };

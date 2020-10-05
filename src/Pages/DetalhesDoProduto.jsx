7673c19be2fc70ba2e3926e2f9a1873e5d735029
Flavianny Maciel <flaviannymaciel@gmail.com>
2020-10-05 13:53:12 -0300

    Em andamento req11-teste passando porém falta campo nota

--- src/Pages/DetalhesDoProduto.jsx
+++ src/Pages/DetalhesDoProduto.jsx
@@ -1,22 +1,25 @@
 import React, { Component } from 'react';
+import Formulario from '../Components/Formulario';
 
 class DetalhesDoProduto extends Component {
-  constructor(props) {
-    super()
+  constructor() {
+    super();
 
-    this.state = {
-
-    }
+    this.state = {};
   }
+
   render() {
     // console.log(this.props.location.state);
     const { title, thumbnail, price } = this.props.location.state.produto;
     return (
       <div className="detalhe-produto">
         <h4 data-testid="product-detail-name">{ title }</h4>
-        <img src={ thumbnail } />
+        <img src={ thumbnail } alt="produto" />
         <p>R$ { price }</p>
-      </div >
+        <div>
+          <Formulario />
+        </div>
+      </div>
     );
   }
 }

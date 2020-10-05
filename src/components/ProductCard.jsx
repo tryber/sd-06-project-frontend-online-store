b33815d415d2fa2cef1e49b27e12059ffeeb6b00
HCaetano <hrcgeo@gmail.com>
2020-10-05 19:36:58 -0300

    fix(ProductCard): refactor ProductCard logic to use localStorage

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -6,30 +6,49 @@ export default class ProductCard extends Component {
 
     this.state = {
       productId: props.product.id,
+      title: props.product.title,
+      price: props.product.price,
+      thumbnail: props.product.thumbnail,
+      quantity: 1,
     };
 
-    this.onClick = this.onClick.bind(this);
+    this.addToCart = this.addToCart.bind(this);
   }
 
-  onClick() {
-    const callback = this.props.callback;
+  addToCart() {
+    const obj = {
+      title: this.state.title,
+      price: this.state.price,
+      thumbnail: this.state.thumbnail,
+      quantity: this.state.quantity,
+    };
 
-    callback(this.state.productId);
+    if (!localStorage.getItem('cart')) {
+      const array = [];
+      array.push(obj);
+      localStorage.clear();
+      localStorage.setItem('cart', JSON.stringify(array));
+    } else { 
+      const save = JSON.parse(localStorage.getItem('cart'));
+      save.push(obj);
+      localStorage.clear();
+      localStorage.setItem('cart', JSON.stringify(save));
+    }
   }
-
+  
   render() {
-    const { title, price, thumbnail } = this.props.product;
+    const { title, price, thumbnail, quantity } = this.props.product;
 
     return (
       <article data-testid="product">
         <div data-testid="shopping-cart-product-name">{title}</div>
         <div>{price}</div>
-        {/* <div data-testid="shopping-cart-product-quantity">{quantity}</div> */}
+        <div data-testid="shopping-cart-product-quantity">{quantity}</div>
         <img src={thumbnail} alt={title}/>
         <button
           type="submit"
           data-testid="product-add-to-cart"
-          onClick={ this.onClick }
+          onClick={ this.addToCart }
         >
           Adicionar ao Carrinho
         </button>

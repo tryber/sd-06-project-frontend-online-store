2184f5f9909bc1c16ebde681842973e94d37388c
Lais Gurgel <laisgurgelf@gmail.com>
2020-10-05 13:30:07 -0300

    Requirement 10: adding CSS

--- src/components/ShoppingCart.js
+++ src/components/ShoppingCart.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import EmptyBox from '../images/empty-box.png';
+import './ShoppingCart.css';
 
 class ShoppingCart extends React.Component {
   render() {
     return (
       <div>
-        <img src={ EmptyBox } alt="empty-box" />
+        <img src={ EmptyBox } alt="empty-box" className="empty-box" />
         <p data-testid="shopping-cart-empty-message">Seu carrinho está vazio</p>
       </div>
     );

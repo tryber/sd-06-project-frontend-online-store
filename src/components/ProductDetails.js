658bcf0e267d1d6c43fadf627c4e55c7a41ae98d
Mariano Ariel Diaz Elsztain <made@poli.ufrj.br>
2020-10-05 17:05:53 -0300

    Added submit button

--- src/components/ProductDetails.js
+++ src/components/ProductDetails.js
@@ -54,6 +54,9 @@ class ProductDetails extends React.Component {
                 onChange={ this.handleReviewChange }
               />
             </label>
+            <button type="submit">
+              Enviar avaliação
+            </button>
           </form>
         </div>
       </div>

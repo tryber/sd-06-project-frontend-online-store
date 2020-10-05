7d69a1d9984cbe09ead5506261c37fc0f0a98868
Victor-Mateus-Ferreira <victormateuslf@gmail.com>
2020-10-05 16:38:05 -0300

    :sparkles: function to add product cart

--- src/components/SearchedItems.js
+++ src/components/SearchedItems.js
@@ -1,22 +1,42 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
+import Cart from '../pages/Cart';
 
 class SearchedItems extends Component {
+  constructor() {
+    super()
+    this.handleclick = this.handleclick.bind(this);
+  }
+
+  handleclick() {
+    const { item: { title }, addTocart } = this.props;
+    addTocart(title);
+  }
+
   render() {
-    const { item: { id, title, thumbnail, price }, query } = this.props;
+    const { item: { id, title, thumbnail, price }, query, addTocart } = this.props;
 
     return (
       <div data-testid="product">
         <h2>{title}</h2>
         <img src={ thumbnail } alt="thumbnail" />
         <span>{price}</span>
-        <Link
-          data-testid="product-detail-link"
-          to={ { pathname: `/card/${id}`, state: { data: query } } }
+        <div>
+          <Link
+            data-testid="product-detail-link"
+            to={ { pathname: `/card/${id}`, state: { data: query } } }
+          >
+            Detalhes
+          </Link>
+        </div>
+        <button
+          type="button"
+          data-testid="product-add-to-cart"
+          onClick={ this.handleclick }
         >
-          Detalhes
-        </Link>
+          Adicionar ao carrinho
+        </button>
       </div>
     );
   }
@@ -30,6 +50,7 @@ SearchedItems.propTypes = {
     price: PropTypes.number.isRequired,
   }).isRequired,
   query: PropTypes.string.isRequired,
+  addTocart: PropTypes.func.isRequired,
 };
 
 export default SearchedItems;

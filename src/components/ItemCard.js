831330054f331ab1506a425d1e699ea7ca92403c
Elisa <elisatcamillo@gmail.com>
2020-10-05 18:55:54 -0300

    Requirement 08: initial function

--- src/components/ItemCard.js
+++ src/components/ItemCard.js
@@ -1,18 +1,28 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
+import ShoppingCart from './ShoppingCart';
 
 class ItemCard extends React.Component {
   render() {
     const { product } = this.props;
     return (
       <div className="product-card" data-testid="product">
-        <h2>{ product.title }</h2>
-        <img src={ product.thumbnail } alt="foto do produto" />
-        <p>{ product.price }</p>
-        <Link data-testid="product-detail-link" to={ `/product-details/${product.id}` }>
+        <h2>{product.title}</h2>
+        <img src={product.thumbnail} alt="foto do produto" />
+        <p>{product.price}</p>
+        <Link data-testid="product-detail-link" to={`/product-details/${product.id}`}>
           <button type="button">DETALHES</button>
         </Link>
+        <button
+          data-testid="product-add-to-cart"
+          onClick={() => {
+            ShoppingCart.addProduct(product);
+          }}
+          type="button"
+        >
+          Adicionar ao carrinho
+        </button>
       </div>
     );
   }
--- src/components/ShoppingCart.js
+++ src/components/ShoppingCart.js
@@ -2,15 +2,57 @@ import React from 'react';
 import EmptyBox from '../images/empty-box.png';
 import './ShoppingCart.css';
 
+const productAdded = [];
+
+const addProduct = (product) => {
+  productAdded.push(product);
+  console.log(productAdded)
+};
+
+
 class ShoppingCart extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      shoppingCart: [],
+      total: 0,
+    };
+    this.addToCart = this.addToCart.bind(this);
+    this.addOne = this.addOne.bind(this);
+    this.removeOne = this.removeOne.bind(this);
+    this.removeProduct = this.removeProduct.bind(this);
+  }
+
+  componentDidMount() {
+    console.log(productAdded);
+    this.addToCart(productAdded);
+  }
+
+  adddToCart(item) {
+    const { shoppingCart } = this.state;
+    const array = [];
+    let productCard;
+    item.forEach((item) => {
+      productCard = {
+        id: item.id,
+        title: item.title,
+        thumbnail: item.thumbnail,
+        price: item.price,
+        quantity: 1,
+      };
+      array.push(productCard);
+      this.setState({ shoppingCart: [...shoppingCart, ...array] });
+    });
+  }
+
   render() {
     return (
       <div>
-        <img src={ EmptyBox } alt="empty-box" className="empty-box" />
+        <img src={EmptyBox} alt="empty-box" className="empty-box" />
         <p data-testid="shopping-cart-empty-message">Seu carrinho está vazio</p>
       </div>
     );
   }
 }
 
-export default ShoppingCart;
+export default { ShoppingCart, addProduct };

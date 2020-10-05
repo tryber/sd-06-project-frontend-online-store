60a3e3d6db591ce2a703308e94c8df4f3353dea9
Daniel Menescal <danielmjales@bct.ect.ufrn.br>
2020-10-05 15:08:09 -0300

    start requiriment 7

--- src/components/ListCard.js
+++ src/components/ListCard.js
@@ -1,15 +1,18 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
 class ListCard extends Component {
   render() {
     const { iten } = this.props;
-    const { title, thumbnail, price } = iten;
+    const { title, thumbnail, price, id } = iten;
     return (
       <div data-testid="product">
-        <h1>{title}</h1>
-        <span>{price}</span>
-        <img src={ thumbnail } alt=" Product" />
+        <Link to={ `/ProductDetails/${id}` }>
+          <h1>{title}</h1>
+          <span>{price}</span>
+          <img src={ thumbnail } alt=" Product" />
+        </Link>
       </div>
     );
   }
@@ -20,6 +23,7 @@ ListCard.propTypes = {
     title: PropTypes.string,
     price: PropTypes.number,
     thumbnail: PropTypes.string,
+    id: PropTypes.string,
   }).isRequired,
 };
 
---
+++ src/pages/ProductDetails.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+class ProductDetails extends Component {
+  render() {
+    return (
+      <div>
+        <p>Oi</p>
+      </div>
+    );
+  }
+}
+
+export default ProductDetails;

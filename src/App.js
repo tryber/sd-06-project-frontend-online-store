c1094453de9fae6770ca2ac5efabd96ff6aa314f
Renan Cunha <renan2610@gmail.com>
2020-10-05 15:44:52 -0300

    Adding route do ProductsDetails component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 import './App.css';
 import Home from './pages/Home';
+import ProductsDetails from './pages/ProductsDetails';
 import ShoppingCart from './pages/ShoppingCart';
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
     <div>
       <BrowserRouter>
         <Switch>
+          <Route path="/products/:id" component={ ProductsDetails } />
           <Route path="/shopping-cart" component={ ShoppingCart } />
           <Route path="/" component={ Home } />
         </Switch>

4c869113626af7f8d2b00a76cb5102728d553286
Elisa <elisatcamillo@gmail.com>
2020-10-05 23:54:46 -0300

    Requirements 08 and 09: linter

--- src/components/ProductDetails.js
+++ src/components/ProductDetails.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import Rater from 'react-rater';
 import 'react-rater/lib/react-rater.css';
 import ShoppingCart from './ShoppingCart';
+import Header from './Header';
 import './ProductDetails.css';
 
 class ProductDetails extends React.Component {
@@ -32,37 +33,39 @@ class ProductDetails extends React.Component {
     const { product } = this.state;
     const { title, price, thumbnail } = product;
     return (
-      <div className="product-details">
-        <div className="product-info">
-          <img alt="Product" src={ thumbnail } className="product-image" />
-          <h1 data-testid="product-detail-name">{title}</h1>
-          <p>{price}</p>
+      <div>
+        <Header />
+        <div className="product-details">
+          <div className="product-info">
+            <img alt="Product" src={ thumbnail } className="product-image" />
+            <h1 data-testid="product-detail-name">{title}</h1>
+            <p>{price}</p>
+          </div>
+          <div className="rate-form">
+            <hr />
+            <h2>Avaliações</h2>
+            <form>
+              <Rater total={ 5 } rating={ 0 } />
+              <br />
+              <textarea
+                data-testid="product-detail-evaluation"
+                type="text"
+                placeholder="Mensagem (opcional)"
+              />
+              <br />
+              <button type="button">Avaliar</button>
+            </form>
+          </div>
+          <button
+            data-testid="product-detail-add-to-cart"
+            onClick={ () => {
+              ShoppingCart.addProduct(product);
+            } }
+            type="button"
+          >
+            ADICIONAR AO CARRINHO
+          </button>
         </div>
-        <div className="rate-form">
-          <hr />
-          <h2>Avaliações</h2>
-          <form>
-            <Rater total={ 5 } rating={ 0 } />
-            <br />
-            <textarea
-              data-testid="product-detail-evaluation"
-              type="text"
-              placeholder="Mensagem (opcional)"
-            />
-            <br />
-            <button type="button">Avaliar</button>
-          </form>
-        </div>
-        <button
-          data-testid="product-add-to-cart"
-          onClick={ () => {
-            ShoppingCart.addProduct(product);
-          } }
-          type="button"
-        >
-          ADICIONAR AO CARRINHO
-        </button>
-
       </div>
     );
   }

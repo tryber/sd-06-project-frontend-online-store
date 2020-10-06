953271500dc7560099cf96c3427533bd6baf4179
Kesley Lahoz <namielahoz@gmail.com>
2020-10-06 15:34:04 -0300

    requisite 8 test

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -3,6 +3,17 @@ import PropTypes from 'prop-types';
 
 
 class ProductList extends React.Component {
+  constructor() {
+    super();
+    this.addProduct = this.addProduct.bind(this);
+  }
+  
+  addProduct() {
+    const getObj = this.props.location.state;
+    const localStorageLength = localStorage.length;
+    localStorage.setItem(localStorageLength, JSON.stringify(getObj));
+  }
+
   render() {
     const { list } = this.props;
     const empty = 0;
@@ -13,6 +24,11 @@ class ProductList extends React.Component {
           <h3>{ items.title }</h3>
           <img src={ items.thumbnail } alt="Product" />
           <p>{ `R$ ${items.price}` }</p>
+          <button
+            data-testid="product-detail-add-to-cart"
+            onClick={this.addProduct}
+          >adicionar ao carrinho
+          </button>
         </div>
       ))
 

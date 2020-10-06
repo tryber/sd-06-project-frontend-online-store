00536c9da8b84ee551a7a64c4901f2dfb44f5c24
Anderson <andersoni.trybe@gmail.com>
2020-10-06 14:23:16 -0300

    Car Product List base created

---
+++ src/components/CartProductList.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Cartlist extends Component {
+    render() {
+        return (
+            <div className="car-list">
+                Cart item list here
+            </div>
+        )
+    }
+}
+
+export default Cartlist;

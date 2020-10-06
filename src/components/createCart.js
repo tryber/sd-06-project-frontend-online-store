c41376b55b95520a62d395240923b4025f6c8dd6
Matheuscara <matheuscara2000@gmail.com>
2020-10-06 14:35:29 -0300

    Testing

--- src/components/createCart.js
+++ src/components/createCart.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { Redirect } from 'react-router-dom';
 
 export default class CreateCart extends Component {
   constructor(props) {
@@ -16,50 +15,47 @@ export default class CreateCart extends Component {
 
   componentDidMount() {
     const cartLocalStorage = JSON.parse(localStorage.getItem('cartLocal'));
+    const result = cartLocalStorage.reduce((previousValue, next) => previousValue + next.price, 0);
+    this.setStateTotal(result);
+  }
+
+  setStateTotal(element) {
     this.setState({
-      total: cartLocalStorage.reduce(
-        (previousValue, next) => previousValue + next.price,
-        0
-      ),
+      total: element,
     });
   }
 
   removeProduct(target, elementId, price) {
+    const { total } = this.state;
     const product = target.parentNode;
     const cartLocalStorage = JSON.parse(localStorage.getItem('cartLocal'));
-    const removeLocalStorage = cartLocalStorage.filter((element) => {
-      return element.id !== elementId;
-    });
+    const removeLocalStorage = cartLocalStorage.filter((element) => element.id !== elementId);
     const CartLocal = JSON.stringify(removeLocalStorage);
     localStorage.setItem('cartLocal', CartLocal);
     product.remove();
-    this.setState({
-      total: parseInt(Math.round(this.state.total - price)),
-    });
+    const result = Math.round(total - price);
+    this.setStateTotal(result);
   }
 
   sumProduct(target, element, price) {
     const number = parseInt(target.parentNode.lastChild.innerText);
-    const result = number + 1;
-    document.getElementById(element).innerText = result;
-    document.getElementById(`${element}price`).innerText = price * result;
-    this.setState({
-      total: Math.round(price + this.state.total),
-    });
+    const quandity = number + 1;
+    document.getElementById(element).innerText = quandity;
+    document.getElementById(`${element}price`).innerText = price * quandity;
+    const result = Math.round(price + this.state.total);
+    this.setStateTotal(result);
   }
+
   decreaseProduct(target, element, price) {
     const number = parseInt(target.parentNode.lastChild.innerText);
-    const result = number - 1;
-    if (result === 0) {
-      this.setState({
-        total: parseInt(Math.round(this.state.total - price)),
-      });
+    const quandity = number - 1;
+    const result = (Math.round(this.state.total - price));
+    if (quandity === 0) {
+      this.setStateTotal(result);
       this.removeProduct(target, element, price);
     } else {
-      document.getElementById(element).innerText = result;
-      this.setState({
-        total: Math.round(this.state.total - price),
-      });
+      document.getElementById(element).innerText = quandity;
+      this.setStateTotal(result);
     }
   }
 
@@ -68,8 +64,8 @@ export default class CreateCart extends Component {
   }
 
   render() {
-    const { cart } = this.props;
-    return this.state.total === 0 ? (
+    const { cart, total } = this.props;
+    return total === 0 ? (
       <div>
         <h1 data-testid="shopping-cart-empty-message">
           Seu carrinho está vazio
@@ -78,40 +74,34 @@ export default class CreateCart extends Component {
     ) : (
       <div data-testid="shopping-cart-empty-message">
         {cart.map((element) => (
-          <div key={element.id}>
-            <img src={element.thumbnail} alt={element.title} />
+          <div key={ element.id }>
+            <img src={ element.thumbnail } alt={ element.title } />
             <h4 data-testid="shopping-cart-product-name">{element.title}</h4>
             <button
-              onClick={(e) =>
-                this.removeProduct(e.target, element.id, element.price)
-              }
+              onClick={ (e) => this.removeProduct(e.target, element.id, element.price) }
             >
               [X]
             </button>
-            <p id={`${element.id}price`}>{element.price}</p>
+            <p id={ `${element.id}price` }>{element.price}</p>
             <button
               data-testid="product-increase-quantity"
-              onClick={(e) =>
-                this.sumProduct(e.target, element.id, element.price)
-              }
+              onClick={ (e) => this.sumProduct(e.target, element.id, element.price) }
             >
               +
             </button>
             <button
               data-testid="product-decrease-quantity"
-              onClick={(e) =>
-                this.decreaseProduct(e.target, element.id, element.price)
-              }
+              onClick={ (e) => this.decreaseProduct(e.target, element.id, element.price) }
             >
               -
             </button>
-            <p id={element.id} data-testid="shopping-cart-product-quantity">
+            <p id={ element.id } data-testid="shopping-cart-product-quantity">
               1
             </p>
           </div>
         ))}
         <div>{this.state.total}</div>
-        <button type="button" onClick={this.removeLocalStorage}>
+        <button type="button" onClick={ this.removeLocalStorage }>
           Deletar Todos
         </button>
       </div>
--- src/pages/shoppingCart.js
+++ src/pages/shoppingCart.js
@@ -13,7 +13,7 @@ export default class shoppingCart extends Component {
   componentDidMount() {
     this.localStorageMount();
   }
-  
+
   async localStorageMount() {
     const cartLocalStorage = await JSON.parse(
       localStorage.getItem('cartLocal'),
@@ -25,7 +25,7 @@ export default class shoppingCart extends Component {
 
   render() {
     const { cart } = this.state;
-    return(
+    return (
       <div>
         <h1>Seu Carrinho:</h1>
         <CreateCart localStorageMount={ this.localStorageMount } cart={ cart } />

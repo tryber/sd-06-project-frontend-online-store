4af5fbfd40c2b918ccc0b08e470adfd87f17dc3d
Stefano <stefano.branz@gmail.com>
2020-10-06 18:43:22 -0300

    CSS changes shop cart and close purchase

--- src/pages/ClosePurchase.jsx
+++ src/pages/ClosePurchase.jsx
@@ -26,66 +26,65 @@ render () {
   const products = this.props.location.state.products;
   return (
     <div>
+      <h2>Itens Selecionados</h2>
       <div>
         {products.length > 0 ? products.map((item) => this.showCart(item)) : this.emptyCart() }
       </div>
-
-      <form>
-        <h2>Informações do comprador</h2>
-        <label className="name-container" htmlFor="checkout-fullname">
-          <input
-            type="text"
-            name="checkout-fullname"
-            data-testid="checkout-fullname"
-            placeholder="Nome Completo"
-          />
-        </label>
-        
-        <label className="cpf-container" htmlFor="checkout-cpf">
-          <input
-            type="text"
-            name="checkout-cpf"
-            data-testid="checkout-cpf"
-            placeholder="CPF"
-          />        
-        </label>        
-        <label className="email-container" htmlFor="checkout-email">
-          <input
-            type="text"
-            name="checkout-email"
-            data-testid="checkout-email" 
-            placeholder="Email"
-            // onChange={(event) => this.updateState('email', event.target.value)}
-          />
-        </label>
-        <label className="phone-container" htmlFor="checkout-phone">
-          <input
-            type="text"
-            name="checkout-phone"
-            data-testid="checkout-phone"
-            placeholder="Telefone"
-          />
-        </label>
-        <label className="cep-container" htmlFor="checkout-cep">
-          <input
-            type="text"
-            name="checkout-cep"
-            data-testid="checkout-cep"
-            placeholder="CEP"
-          />
-        </label>
-        <label className="address-container" htmlFor="checkout-address">
-          <input
-            type="text"
-            name="checkout-address"
-            data-testid="checkout-address"
-            placeholder="Endereço"
-          />
-        </label>                 
-
-      </form>
-      <div>
-        <h2>Método de pagamento</h2>
+      <div className="buyer-informations">
+        <form>
+          <h2>Informações do comprador</h2>
+          <label className="name-container" htmlFor="checkout-fullname">
+            <input
+              type="text"
+              name="checkout-fullname"
+              data-testid="checkout-fullname"
+              placeholder="Nome Completo"
+            />
+          </label>
+          <label className="cpf-container" htmlFor="checkout-cpf">
+            <input
+              type="text"
+              name="checkout-cpf"
+              data-testid="checkout-cpf"
+              placeholder="CPF"
+            />        
+          </label>        
+          <label className="email-container" htmlFor="checkout-email">
+            <input
+              type="text"
+              name="checkout-email"
+              data-testid="checkout-email" 
+              placeholder="Email"
+            />
+          </label>
+          <label className="phone-container" htmlFor="checkout-phone">
+            <input
+              type="text"
+              name="checkout-phone"
+              data-testid="checkout-phone"
+              placeholder="Telefone"
+            />
+          </label>
+          <label className="cep-container" htmlFor="checkout-cep">
+            <input
+              type="text"
+              name="checkout-cep"
+              data-testid="checkout-cep"
+              placeholder="CEP"
+            />
+          </label>
+          <label className="address-container" htmlFor="checkout-address">
+            <input
+              type="text"
+              name="checkout-address"
+              data-testid="checkout-address"
+              placeholder="Endereço"
+            />
+          </label>                 
+        </form>
+      </div>
+      <h2>Método de pagamento</h2>
+      <div className="buyer-informations">
         <label htmlFor="payment">
           <input name="payment" id="boleto" value="Boleto" type="radio" /> 
              <img src={boleto} alt="boleto" width="50px" />
@@ -102,7 +101,7 @@ render () {
       </div>
 
   
-      <button>Comprar</button>
+      <button className="btn btn-green">Comprar</button>
 
     </div>
 

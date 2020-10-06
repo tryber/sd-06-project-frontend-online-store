3aee13fb2c788d490b8aa9d01e52b332adff13f2
Anderson <andersoni.trybe@gmail.com>
2020-10-06 14:22:51 -0300

    Chopping Cart page base

--- src/pages/ShoppingCart.jsx
+++ src/pages/ShoppingCart.jsx
@@ -1,16 +1,21 @@
 import React from 'react';
+import Cartlist from '../components/CartProductList';
 
 
 class ShoppingCart extends React.Component {
   constructor() {
     super()
-
   }
 
   render() {
     return (
       <section>
         <p data-testid="shopping-cart-empty-message">Seu carrinho está vazio</p>
+        <Cartlist />
+        <section>
+          <p>Total: </p>
+          <button>Finalizar Compra</button>
+        </section>
       </section>
     )
   }

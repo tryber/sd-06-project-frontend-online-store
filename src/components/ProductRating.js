cc901bfc4fddc163e4db0b3d8243b1d593648671
Ana Higo <anahigo.arq@gmail.com>
2020-10-07 00:09:11 -0300

    :construction: test ok, list comments ok (no stars appear), missing LocalStorage

--- src/components/ProductRating.js
+++ src/components/ProductRating.js
@@ -3,7 +3,42 @@ import Rater from 'react-rater';
 import 'react-rater/lib/react-rater.css';
 
 class ProductRating extends React.Component {
+  constructor() {
+    super();
+    this.handleChange = this.handleChange.bind(this);
+    this.handleButtonComment = this.handleButtonComment.bind(this);
+    this.state = {
+      email: '',
+      rating: '',
+      message: '',
+      register: [],
+    };
+  }
+
+  handleChange({ target }) {
+    const { name, value } = target;
+    this.setState({ [name]: value });
+  }
+
+  registerComments(comment) {
+    const { register } = this.state;
+    this.setState({
+      register: register.concat(comment),
+    });
+  }
+
+  handleButtonComment() {
+    const { email, rating, message } = this.state;
+    const comment = {
+      email,
+      rating,
+      message,
+    };
+    this.registerComments(comment);
+  }
+
   render() {
+    const { register } = this.state;
     return (
       <div>
         <form>
@@ -12,23 +47,43 @@ class ProductRating extends React.Component {
             <input
               type="email"
               placeholder="Email"
+              name="email"
+              onChange={ this.handleChange }
             />
-            <Rater total={ 5 } rating={ 0 } />
+            <Rater
+              total={ 5 }
+              rating={ 0 }
+              onChange={ this.handleChange }
+            />
             <br />
             <br />
             <textarea
               data-testid="product-detail-evaluation"
               placeholder="Mensagem (opcional)"
+              name="message"
               rows="5"
               cols="50"
+              onChange={ this.handleChange }
             />
             <br />
             <br />
-            <button type="button">
+            <button
+              type="button"
+              onClick={ this.handleButtonComment }
+            >
               Avaliar
             </button>
           </fieldset>
         </form>
+        <div>
+          {register.map((comment) => (
+            <div key={ comment.email }>
+              <p>{ comment.email }</p>
+              <p>{ comment.rating }</p>
+              <p>{ comment.message }</p>
+            </div>
+          ))}
+        </div>
       </div>
     );
   }

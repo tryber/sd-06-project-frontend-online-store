171302c7e230e54d035c86794d7253ca7f137e37
Anderson <andersoni.trybe@gmail.com>
2020-10-08 14:42:00 -0300

    Alterado as propTypes

--- src/pages/ShoppingCart.jsx
+++ src/pages/ShoppingCart.jsx
@@ -1,7 +1,5 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import PropTypes from 'prop-types';
-
 
 class ShoppingCart extends React.Component {
   constructor(props) {
@@ -54,7 +52,5 @@ class ShoppingCart extends React.Component {
   }
 
 }
-
-ShoppingCart.propTypes = { cartItems: PropTypes.array }
 
 export default ShoppingCart;

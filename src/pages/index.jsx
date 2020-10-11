646d9b3c719f1e63b019bf82179d99617d4b2d5b
HCaetano <hrcgeo@gmail.com>
2020-10-11 17:11:58 -0300

    feat(index): add Checkout export

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,3 +1,4 @@
 export { default as Home } from './Home';
 export { default as ShoppingCart } from './ShoppingCart';
 export { default as ProductDetails } from './ProductDetails';
+export { default as Checkout } from './Checkout';

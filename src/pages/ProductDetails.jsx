1ea0049e95641f949d5892639a4a63b54b094ba3
pedrobth <51889535+pedrobth@users.noreply.github.com>
2020-10-12 17:25:55 -0300

    Update src/pages/ProductDetails.jsx
    Co-authored-by: Havyner Caetano <20730250+HCaetano@users.noreply.github.com>

--- src/pages/ProductDetails.jsx
+++ src/pages/ProductDetails.jsx
@@ -33,7 +33,6 @@ export default class ProductDetails extends React.Component {
   async componentDidMount() {
     const { productId, category, title } = this.props.match.params;
     const productFromId = await api.getProductsFromId(productId);
-    console.log(productFromId);
     const productList = await api.getProductsFromCategoryAndQuery(category, title);
     const productFilter = productList.results.filter(product => product.id === productId)[0];
     const product = productFilter ? productFilter : productFromId;
